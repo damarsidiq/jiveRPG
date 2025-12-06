fd59526306fe9c1621275b1505b6d683b28580eb
Erwanto Damarsidiq <45053494+damarsidiq@users.noreply.github.com>
2025-12-06 22:19:36 +0700

    Refactor storyline intro and language handling
    Updated storyline introduction and adjusted image URL handling based on conditions. Enhanced dictionary initialization and translation handling for multiple languages.

--- Subtext.js
+++ Subtext.js
@@ -102,26 +102,30 @@ L'ultima truffa, quando viene ingannato, è quasi misericordiosa: pone fine alla
 `;
 
 var itDiction = `["affatto - at all","ai - to the","al - to the","alienazione - alienation","alla - to the","altri - others","altro - other","ambizione - ambition","ambizioni - ambitions","anche - also","annuncia - announces","articolazione - articulation","artificialità - artificiality","astutamente - cunningly","atto - act","attorno - around","attraverso - through","avuto - had","bisogno - need","c'è - there is","caduto - fallen","calcolo - calculation","calore - warmth","capisco - i understand","carne - flesh","causa - cause","cercare - search","che - that / which / who","chiede - asks","ciò - what","collasso - collapse","colore - color","come - like / as / how","completamente - completely","compresi - understood","comunicano - communicate","comunità - community","con - with","concorda - agrees","condividiamo - we share","connessione - connection","connettere - to connect","conosce - knows","contare - to count","contorni - outlines","contraddizioni - contradictions","controllo - control","conversazioni - conversations","cose - things","così - so","costruita - built","cui - to whom","culmine - culmination","da - from","dà - gives","dall'essere - from being","davvero - really","definito - defined","del - of the (masculine singular)","della - of the (feminine singular)","delle - of the","deriva - derives","deve - must","di - of / from","dicono - they say","diffusi - widespread","digitale - digital","dirlo - to say","disegna - draws","distopico - dystopian","diventa - becomes","diventare - to become","documentario - documentary","due - two","e - and","è - is","eccessivo - excessive","entrambi - both","erode - erodes","esattamente - exactly","esempi - examples","esfoliazione - exfoliation","esperienza - experience","esplicitamente - explicitly","essere - to be","facendo - making","fanno - they do","fare - to do","fatto - fact","fiammifero - matchstick","figure - figures","film - film","fine - end","focalizzati - focused","focus - focus","fondo - background","forma - form","formano - form","freddamente - coldly","fuoco - fire","futilità - futility","giusto - right","gli - the","grezzo - raw","guarda - looks","ha - has","hai - you have","ho - i","idea - idea","identità - identity","il - the (masculine singular)","illuminata - lit","implica - implies","incredibile - incredible","ingannando - deceiving","ingannato - deceived","insinua - insinuates","intelligenza - intelligence","intensi - intense","intera - whole","interculturale - intercultural","interiore - inner","intimità - intimacy","intitola - is titled","io - i","koan - koan","l'astuzia - the cunning","l'autonomia - autonomy","l'avere - having","l'empatia - empathy","l'esperienza - the experience","l'essere - the being","l'inganno - the deception","l'insistenza - the insistence","l'intelletto - the intellect","l'ironia - the irony","l'ocd - the ocd","l'ultima - the last","la - the (feminine singular)","le - the","leggono - they read","lente - lens","letteralmente - literally","letto - bed","lettura - reading","liberatoria - release","libero - free","linguaggio - language","lo - the","loro - their","luci - lights","lunghi - long","lungi - far","ma - but","mai - never","manipolazione - manipulation","matchstick - matchstick","meccanica - mechanics","meccanici - mechanics","meccanico - mechanical","meccanismo - mechanism","men - men","messaggio - message","metà - half","metafora - metaphor","mina - mine","misericordiosa - merciful","modo - way","mondo - world","morale - moral","nascosti - hidden","ne - you","nei - in","nel - in","nemmeno - not even","nessuno - no one","network - network","non - not","o - or","ogni - every","oppure - or","ossessione - obsession","ostensibilmente - ostensibly","ovvio - obvious","parla - speaks","pensare - think","pensi - think","per - for / to / in order to","percepisci - you perceive","perché - because","perde - loses","performance - performance","persino - even","personaggi - characters","personaggio - character","persone - people","piani - plans","piccoli - small","più - more","pone - places","possiamo - we can","possono - can","potrebbe - could","precisione - precision","prendi - take","prigione - prison","priva - devoid","profonda - deep","profondo - deep","pronunciato - pronounced","pulire - to clean","punizione - punishment","puntano - aim","puoi - you can","qualche - some","qualcosa - something","quale - which","quando - when","quanto - how much","quasi - almost","quegli - those","quel - that","quella - that","questa - this","questo - this","quiete - calm","quindi - so","radiazione - radiation","reale - real","reali - real","recitando - acting","reminiscenza - reminiscence","rendere - make","resistenza - resistance","ribellione - rebellion","riferimento - reference","riguarda - concerns","riguardava - was about","rimane - remains","rimanere - to remain","rinforzato - reinforced","risiede - resides","rispecchia - reflects","ritmo - rhythm","rivedi - review","routine - routine","roy - roy","sangue - blood","sbiadito - faded","scena - scene","scoperto - discovered","se - if","sembra - seems","semplice - simple","sente - feels","senti - feel","senza - without","sfocate - blurred","si - it","sì - yes","significa - means","silenzi - silences","silenziosamente - silently","simboleggiato - symbolized","simbolico - symbolic","sistemare - to fix","slang - slang","social - social","sole - sun","solito - usual","solo - only","sono - are","sorta - sort","sottile - subtle","sottili - subtle","sottostante - underlying","sottotesti - subtexts","sottotesto - subtext","specialmente - especially","speranza - hope","spesso - often","spirituale - spiritual","sta - is","stanza - room","stati - states","stessa - same","stesso - same","storia - history","stranezza - strangeness","strato - layer","struttura - structure","sua - his","successo - success","sul - on","sull'infertilità - on infertility","sulla - on","suo - his","superficie - surface","sussurrano - whisper","svegliarsi - wake up","svelare - to reveal","telecamera - camera","tema - theme","the - the","thriller - thriller","tic - tic","titolo - title","tokyo - tokyo","tono - tone","tradimento - betrayal","tradurre - to translate","trama - plot","transitoria - transitory","trappole - traps","tratta - deals with","troppo - too","trucco - trick","truffa - scam","truffato - scammed","truffatore - scammer","truffatori - scammers","tutti - everyone","tutto - everything","udite - heard","ulteriormente - further","umano - human","un - a / an (masculine)","un'eco - an echo","un'illusione - an illusion","un'imitazione - an imitation","un'improvvisa - a sudden","un'interpretazione - an interpretation","una - a / an (feminine)","vedere - to see","vedi - see","verità - truth","vero - true","versione - version","verso - towards","vetrosa - glassy","vicina - close","vicino - close","viene - comes","visivo - visual","vissuta - experienced","vita - life"]`;
-var iLM = `{"a":{"start":0,"end":17},"b":{"start":18,"end":18},"c":{"start":19,"end":52},"d":{"start":53,"end":73},"e":{"start":74,"end":84},"è":{"start":75,"end":75},"f":{"start":85,"end":100},"g":{"start":101,"end":104},"h":{"start":105,"end":107},"i":{"start":108,"end":124},"k":{"start":125,"end":125},"l":{"start":126,"end":152},"m":{"start":153,"end":169},"n":{"start":170,"end":177},"o":{"start":178,"end":183},"p":{"start":184,"end":213},"q":{"start":214,"end":226},"r":{"start":227,"end":246},"s":{"start":247,"end":300},"t":{"start":301,"end":322},"u":{"start":323,"end":332},"v":{"start":333,"end":345}}`;
+var itLM = `{"a":{"start":0,"end":17},"b":{"start":18,"end":18},"c":{"start":19,"end":52},"d":{"start":53,"end":73},"e":{"start":74,"end":84},"è":{"start":75,"end":75},"f":{"start":85,"end":100},"g":{"start":101,"end":104},"h":{"start":105,"end":107},"i":{"start":108,"end":124},"k":{"start":125,"end":125},"l":{"start":126,"end":152},"m":{"start":153,"end":169},"n":{"start":170,"end":177},"o":{"start":178,"end":183},"p":{"start":184,"end":213},"q":{"start":214,"end":226},"r":{"start":227,"end":246},"s":{"start":247,"end":300},"t":{"start":301,"end":322},"u":{"start":323,"end":332},"v":{"start":333,"end":345}}`;
+
+var itSND = ['Fit.mp3',[0.000000,19.226473,0],[19.903524,37.408224,1],[37.964213,42.986053,2],[43.618267,47.008009,3],[47.617804,54.182066,4],[54.863601,62.414296,5],[63.077897,67.705163,6],[68.207347,71.884052,7],[72.404171,79.111914,8],[79.703774,82.501656,9],[83.039710,86.142490,10],[86.806090,91.863800,11],[92.509466,97.064992,12],[97.746527,103.898281,13],[104.526011,110.462543,14],[111.162014,116.004502,15],[116.524621,120.201326,16],[120.864926,128.397686,17],[129.079221,133.096693,18],[133.760293,137.544609,19],[138.172339,141.741432,20],[142.333292,151.300863,21],[151.892723,153.901459,22],[154.547124,160.609202,23],[161.254867,166.294642,24],[166.904437,172.554007,25],[173.199672,176.212776,26],[176.876376,181.808540,27],[182.400400,188.552154,28],[189.215754,197.609401,29],[198.273001,208.980282,30],[209.643882,212.890143,31],[213.464067,216.889679,32],[217.535345,221.319660,33],[222.001195,225.803445,34],[226.502916,232.242161,35],[232.869891,239.649375,36],[240.169494,243.577172,37],[244.330448,248.329984,38],[248.975649,255.773069,39],[256.418734,259.413902,40],[260.077503,264.417807,41],[265.081408,271.555994,42],[272.219594,276.488158,43],[277.115888,284.720389,44],[285.348119,294.190144,45],[294.746133,297.526080,46],[298.082070,304.180018,47],[304.700138,311.282335,48]];
+
 
 /*things to adjust*/
 var EHIMGURL;
 if(storyline.gtUrl.indexOf('https:') === 0){
-    EHIMGURL = 'https://ik.imagekit.io/pnscgil5d/anc/m/';
+    EHIMGURL = jovuniverse.EHIMGURL+'anc/m/';
 }
 else{
-    EHIMGURL = './jiveRPG/anc/m/';
+    EHIMGURL = storyline.BIMG+'anc/m/';
 }
 const svisual=['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','a.jpg','b.jpg','c.jpg','d.jpg','e.jpg','f.jpg','g.jpg','h.jpg','i.jpg','j.jpg','k.jpg','l.jpg','m.jpg','n.jpg','p.jpg','q.jpg','r.jpg','s.jpg','t.jpg','u.jpg','v.jpg','w.jpg','x.jpg','y.jpg','z.jpg','1a.jpg','1b.jpg','1c.jpg','1d.jpg','1e.jpg','1f.jpg','1g.jpg'];
 const ImgRo = 1;
 const oImg = 'o.jpg';
 const ehbmtitle = 'subtext';
 
-storyline.intro = `<p>A conversation on the subject of movies subtext.</p>
+storyline.intro = `<p>A conversation on the subject of movies subtext. 49 sentences total.</p>
 <div style="font-size:smaller;">
-<p>Credits:</p>
+<p>Voice Available:<br>IT</p>
 <p>Story Text:<br><a href="https://www.chatgpt.com" target="_blank">ChatGPT</a></p>
-<p>Translation:<ul><li><a href="https://www.easemate.ai/ai-translator" target="_blank">EaseMate AI</a></li></ul></p>
+<p>Voices:<br><a href="hhttps://voicertool.com/" target="_blank">Voicertool</a></p>
+<p>Translation:<br><a href="https://www.easemate.ai/ai-translator" target="_blank">EaseMate AI</a></p>
 <p>Images:<br><ul><li><a href="https://www.rawpixel.com" target="_blank">rawpixel.com</a></li><li><a href="https://www.pixabay.com" target="_blank">pixabay.com</a></li></ul></p></div>`;
 
 storyline.lang = ['EN','IT'];
@@ -171,9 +175,49 @@ var reperc = {
             }
         }
     },
+    avdi:0,
     initDictionary:function(){
-        itDiction = JSON.parse(itDiction);
-        reperc.iLM = JSON.parse(iLM);
+       if(typeof itDiction !== 'undefined' && itDiction!=''){
+            itDiction = JSON.parse(itDiction);
+            reperc.itLM = JSON.parse(itLM); reperc.avdi++;
+        }        
+        if(typeof frDiction !== 'undefined' && frDiction!=''){
+          frDiction = JSON.parse(frDiction);
+          reperc.frLM = JSON.parse(frLM);reperc.avdi++;
+        }        
+        if(typeof deDiction !== 'undefined' && deDiction!=''){
+          deDiction = JSON.parse(deDiction);
+          reperc.deLM = JSON.parse(deLM);reperc.avdi++;
+        }        
+        if(typeof esDiction !== 'undefined' && esDiction!=''){
+          esDiction = JSON.parse(esDiction);
+          reperc.esLM = JSON.parse(esLM);reperc.avdi++;
+        }
+        storyline.snd = {};
+        if(typeof frSND !== 'undefined' && frSND.length){            
+            storyline.snd.fr = {};
+            storyline.snd.fr.p = storyline.sndURL+frSND[0];
+            frSND.splice(0,1);
+            storyline.snd.fr.t = frSND;
+        }
+        if(typeof esSND !== 'undefined' && esSND.length){            
+            storyline.snd.es = {};
+            storyline.snd.es.p = storyline.sndURL+esSND[0];
+            esSND.splice(0,1);
+            storyline.snd.es.t = esSND;
+        }
+        if(typeof itSND !== 'undefined' && itSND.length){            
+            storyline.snd.it = {};
+            storyline.snd.it.p = storyline.sndURL+itSND[0];
+            itSND.splice(0,1);
+            storyline.snd.it.t = itSND;
+        }
+        if(typeof deSND !== 'undefined' && deSND.length){            
+            storyline.snd.de = {};
+            storyline.snd.de.p = storyline.sndURL+deSND[0];
+            deSND.splice(0,1);
+            storyline.snd.de.t = deSND;
+        }
     },
     initStory:function(){
         reperc.initBookmark();
@@ -227,7 +271,13 @@ var reperc = {
         document.addEventListener('unfoldx', e => {
             reperc.nextHdlr(e.detail);          
         });
-  }
+    },
+    xDict:function(p){
+        $('body').append('<div id="dictionary" style="top:'+(p.top-40)+'px;left:'+p.left+'px;">Not Available</div>').addClass('dictionInq');
+    },
+    wo:function(r){
+      window.open(r, '_blank');
+    }
 };
 var chidx;
 var n;
@@ -248,26 +298,71 @@ config.narration = n;
 chidx = storyline.addChapter(config);
 
 storyline.dictionfn = function(t,l){
-    $('#dictionary').remove();
+   $('#dictionary').remove();
     $('.txtfrg.inq').removeClass('inq');
     const pos = $(l).offset();
     $(l).addClass('inq');
+    if(!reperc.avdi){reperc.xDict(pos);return;}
+
     t = t.toLowerCase().replace(/^[\p{P}\p{S}\s]+|[\p{P}\p{S}\s]+$/gu, '');
     t = t +' - ';
     let xx = t[0].toLowerCase();
 
     if(storyline.clang == 'it'){
-        if (!reperc.iLM[xx]){
-            window.open('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+        if(typeof reperc.itLM === 'undefined'){reperc.xDict(pos);return;}
+        if (!reperc.itLM[xx]){
+            reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
             return;
         }
-        for(var i=reperc.iLM[xx].start;i<=reperc.iLM[xx].end;i++){
+        for(var i=reperc.itLM[xx].start;i<=reperc.itLM[xx].end;i++){
             if(itDiction[i].indexOf(t) === 0){
                 $('body').append('<div id="dictionary" style="top:'+(pos.top-40)+'px;left:'+pos.left+'px;">'+itDiction[i].split(' - ')[1]+'</div>').addClass('dictionInq');
                 return;
             }
         }
-        window.open('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+        reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+    }
+    else if(storyline.clang == 'fr'){
+        if(typeof reperc.frLM === 'undefined'){reperc.xDict(pos);return;}
+        if (!reperc.frLM[xx]){
+            reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+            return;
+        }
+        for(var i=reperc.frLM[xx].start;i<=reperc.frLM[xx].end;i++){
+            if(frDiction[i].indexOf(t) === 0){
+                $('body').append('<div id="dictionary" style="top:'+(pos.top-40)+'px;left:'+pos.left+'px;">'+frDiction[i].split(' - ')[1]+'</div>').addClass('dictionInq');
+                return;
+            }
+        }
+        reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+    }
+    else if(storyline.clang == 'es'){
+        if(typeof reperc.esLM === 'undefined'){reperc.xDict(pos);return;}
+        if (!reperc.esLM[xx]){
+            reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+            return;
+        }
+        for(var i=reperc.esLM[xx].start;i<=reperc.esLM[xx].end;i++){
+            if(esDiction[i].indexOf(t) === 0){
+                $('body').append('<div id="dictionary" style="top:'+(pos.top-40)+'px;left:'+pos.left+'px;">'+esDiction[i].split(' - ')[1]+'</div>').addClass('dictionInq');
+                return;
+            }
+        }
+        reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+    }
+    else if(storyline.clang == 'de'){
+        if(typeof reperc.deLM === 'undefined'){reperc.xDict(pos);return;}
+        if (!reperc.deLM[xx]){
+            reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+            return;
+        }
+        for(var i=reperc.deLM[xx].start;i<=reperc.deLM[xx].end;i++){
+            if(deDiction[i].indexOf(t) === 0){
+                $('body').append('<div id="dictionary" style="top:'+(pos.top-40)+'px;left:'+pos.left+'px;">'+deDiction[i].split(' - ')[1]+'</div>').addClass('dictionInq');
+                return;
+            }
+        }
+        reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
     }
 };
 $('#text5').prop('src',EHIMGURL+oImg);
