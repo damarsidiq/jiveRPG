3b867a4f44a1188621aba6cf11b5994bcaf8e27d
Erwanto Damarsidiq <45053494+damarsidiq@users.noreply.github.com>
2025-12-06 20:28:19 +0700

    Add files via upload

---
+++ Bergman.js
@@ -0,0 +1,427 @@
+var textSt = `Marta is sitting down.
+She is in front of the mirror.
+She is combing her hair.
+She is having difficulty combing her hair.
+Its so unlike any shampoo commercial on television.
+Tomas is sitting down.
+He is in his room.
+He is writing on a piece of paper.
+He seems to enjoy this activity.
+His pen keeps dancing on the paper.
+Words keep streaming down.
+Suddenly, someone knocks on the door.
+Oswald is walking on the beach.
+Karin is walking beside him.
+The sea is indifferent to their presence.
+The waves keeps coming and going.
+The beach feels empty without the sea by its side.
+Even if Oswald and Karin were walking on the beach.
+
+<muoborder>
+Marta sitter ner.  
+Hon är framför spegeln.  
+Hon kammar sitt hår.  
+Hon har svårt att kamma sitt hår.  
+Det är så olikt alla schampo-reklamer på television.  
+Tomas sitter ner.  
+Han är i sitt rum.  
+Han skriver på ett papper.  
+Han verkar njuta av denna aktivitet.  
+Hans penna dansar hela tiden på pappret.  
+Ord fortsätter att strömma ner.  
+Plötsligt knackar någon på dörren.  
+Oswald går på stranden.  
+Karin går bredvid honom.  
+Havet är likgiltigt för deras närvaro.  
+Vågorna fortsätter att komma och gå.  
+Stranden känns tom utan havet vid sin sida.  
+Även om Oswald och Karin gick på stranden.
+
+<muoborder>
+
+Marta sitter ned.  
+Hun er foran speilet.  
+Hun børster håret sitt.  
+Hun har vansker med å børste håret sitt.  
+Det er så ulikt enhver sjampo-reklame på TV.  
+Tomas sitter ned.  
+Han er på rommet sitt.  
+Han skriver på et papir.  
+Han ser ut til å nyte denne aktiviteten.  
+Pennen hans danser stadig på papiret.  
+Ord strømmer nedover.  
+Plutselig banker noen på døren.  
+Oswald går på stranden.  
+Karin går ved siden av ham.  
+Havet er likegyldig til deres tilstedeværelse.  
+Bølgene kommer og går.  
+Stranden føles tom uten havet ved sin side.  
+Selv om Oswald og Karin gikk på stranden.
+
+<muoborder>
+Marta sidder ned.  
+Hun er foran spejlet.  
+Hun reder sit hår.  
+Hun har svært ved at rede sit hår.  
+Det er så anderledes end enhver shampoo-reklame på fjernsynet.  
+Tomas sidder ned.  
+Han er i sit værelse.  
+Han skriver på et stykke papir.  
+Han ser ud til at nyde denne aktivitet.  
+Hans pen danser hele tiden på papiret.  
+Ord strømmer ned.  
+Pludselig banker nogen på døren.  
+Oswald går på stranden.  
+Karin går ved siden af ham.  
+Havet er ligeglad med deres tilstedeværelse.  
+Bølgerne kommer og går.  
+Stranden føles tom uden havet ved sin side.  
+Selv hvis Oswald og Karin gik på stranden.
+
+<muoborder>
+Марта сидит.  
+Она перед зеркалом.  
+Она расчесывает волосы.  
+Ей трудно расчесать волосы.  
+Это совсем не похоже на рекламу шампуня по телевизору.  
+Томас сидит.  
+Он в своей комнате.  
+Он пишет на листе бумаги.  
+Кажется, ему нравится это занятие.  
+Его ручка продолжает танцевать на бумаге.  
+Слова продолжают течь.  
+Вдруг кто-то стучит в дверь.  
+Освальд гуляет по пляжу.  
+Карин идет рядом с ним.  
+Море безразлично к их присутствию.  
+Волны продолжают приходить и уходить.  
+Пляж кажется пустым без моря рядом.  
+Даже если Освальд и Карин гуляли по пляжу.
+
+<muoborder>
+Marta istuu alas.  
+Hän on peilin edessä.  
+Hän harjaa hiuksiaan.  
+Hänellä on vaikeuksia hiustensa harjaamisessa.  
+Se on niin erilaista kuin mikään shampoo-mainos televisiossa.  
+Tomas istuu alas.  
+Hän on huoneessaan.  
+Hän kirjoittaa paperille.  
+Hän näyttää nauttivan tästä toiminnasta.  
+Hänen kynänsä tanssii paperilla.  
+Sanoja virtaa alas.  
+Yhtäkkiä joku koputtaa oveen.  
+Oswald kävelee rannalla.  
+Karin kävelee hänen vierellään.  
+Meri on välinpitämätön heidän läsnäolostaan.  
+Aallot tulevat ja menevät.  
+Ranta tuntuu tyhjältä ilman merta sen vieressä.  
+Vaikka Oswald ja Karin kävelisivätkin rannalla.
+`;
+
+
+
+var svSND = ['Dsv.mp3',[0.000000,1.639374,0],[2.185833,4.371665,1],[4.719411,6.706532,2],[7.103956,9.438822,3],[9.885925,13.313707,4],[13.860166,15.747930,5],[16.393744,18.281509,6],[18.728611,20.815087,7],[21.262190,24.193192,8],[24.739650,27.670653,9],[28.167433,30.701012,10],[31.197792,33.731371,11],[34.321297,36.072447,12],[36.935603,38.779899,13],[39.462971,42.350506,14],[43.232291,45.709982,15],[46.542088,49.584867,16],[50.479071,53.441122,17]];
+var ruSND = ['Dru.mp3',[0.000000,1.248596,0],[2.143816,3.480756,1],[4.346528,6.284208,2],[7.014519,9.069991,3],[9.823860,13.021915,4],[13.669772,14.883030,5],[15.719354,17.179976,6],[18.045748,19.871525,7],[20.666622,23.446515,8],[24.141489,26.997947,9],[27.710589,29.636489,10],[30.343242,32.328039,11],[33.011233,34.707439,12],[35.590879,37.128066,13],[38.040955,40.326121,14],[41.127108,43.800988,15],[44.554857,46.869472,16],[47.699906,50.585812,17]];
+var fiSND = ['Dfi.mp3',[0.000000,1.713130,0],[2.364864,4.028338,1],[4.798005,6.535962,2],[7.231145,10.111190,3],[10.806373,14.505740,4],[15.200923,16.808534,5],[17.571994,19.055465,6],[19.862374,21.569296,7],[22.295514,24.821760,8],[25.560392,27.956291,9],[28.645267,30.377017,10],[31.128063,33.517755,11],[34.268801,36.006759,12],[36.838496,38.861975,13],[39.613021,42.648240,14],[43.337216,45.273798,15],[46.000016,49.295928,16],[50.071802,53.454613,17]];
+
+
+
+/*things to adjust*/
+var EHIMGURL;
+if(storyline.gtUrl.indexOf('https:') === 0){
+    EHIMGURL = jovuniverse.EHIMGURL+'csh/';
+}
+else{
+    EHIMGURL = storyline.BIMG+'csh/';
+}
+const svisual = ['7.jpg','8.jpg','9.jpg','1a.jpg','q.jpg','o.jpg','5.jpg','1.jpg','6.jpg','3.jpg','4.jpg','2.jpg','x.jpg','z.jpg','v.jpg','u.jpg','y.jpg','t.jpg','s.jpg','w.jpg','p.jpg','b.jpg','c.jpg','a.jpg','g.jpg','f.jpg','m.jpg','k.jpg','d.jpg','l.jpg','e.jpg','h.jpg','j.jpg','i.jpg','r.jpg','n.jpg'];
+const ImgRo = 1;
+const oImg = '1a.jpg';
+const ehbmtitle = 'mREV';
+
+
+
+storyline.intro = `<p>18 sentences with basic vocabulary for learning, inspired by 2 of Ingmar Bergman's movies.</p>
+<div style="font-size:smaller;">
+<p>Voice Available:<br>RU,SV,FI</p>
+
+<p>Translations:<ul><li><a href="https://www.easemate.ai/ai-translator" target="_blank">EaseMate AI</a></li></ul></p>
+<p>Voices:<ul>
+<li><a href="https://ttsmaker.com/" target="_blank">TTSMaker</a></li>
+</ul></p>
+<p>Images:<br><ul><li><a href="https://www.pixabay.com" target="_blank">pixabay.com</a></li></ul></p></div>`;
+
+storyline.lang = ['EN','SV','NO','DA','FI','RU'];
+storyline.clang = storyline.deflang = 'sv';
+/*eo things to adjust*/
+
+
+var reperc = {
+    starter:false,
+    storylines:[],
+    starterParagraph:false,
+    bmidx:false,
+    storyvis:svisual,
+    setBookmark:function(x){
+        if(reperc.localstorage == false){return;}
+        reperc.bookmark[reperc.bmidx].cpar = x;
+        reperc.localstorage.setItem('southSideBM',JSON.stringify(reperc.bookmark));
+    },
+    initBookmark:function(){
+        if (typeof localStorage == "object"){
+            reperc.localstorage =  localStorage;
+        } else if (typeof globalStorage == "object"){
+            reperc.localstorage = globalStorage[location.host];
+        } else {
+            reperc.localstorage = false;
+        }
+        if(reperc.localstorage !== false){
+            reperc.bookmark = reperc.localstorage.getItem('southSideBM');
+            if(reperc.bookmark == null){
+                reperc.bookmark = [];
+                reperc.bmidx = 0;
+                reperc.bookmark[0] = {title:ehbmtitle,cpar:0};
+            }
+            else{
+                reperc.bookmark = JSON.parse(reperc.bookmark);
+                for(var i=0;i<reperc.bookmark.length;i++){
+                    if(reperc.bookmark[i].title == ehbmtitle){
+                        reperc.starterParagraph = reperc.bookmark[i].cpar;
+                        reperc.bmidx = i;
+                        break;
+                    }
+                }
+                if(reperc.bmidx === false){
+                    reperc.bmidx = reperc.bookmark.length;
+                    reperc.bookmark[reperc.bmidx] = {title:ehbmtitle,cpar:0};
+                }
+            }
+        }
+    },
+    avdi:0,
+    initDictionary:function(){
+        if(typeof svDiction !== 'undefined' && svDiction!=''){
+          svDiction = JSON.parse(svDiction);
+          reperc.svLM = JSON.parse(svLM);   reperc.avdi++;
+        }        
+        if(typeof noDiction !== 'undefined' && noDiction!=''){
+          noDiction = JSON.parse(noDiction);
+          reperc.noLM = JSON.parse(noLM); reperc.avdi++;
+        }        
+        if(typeof ruDiction !== 'undefined' && ruDiction!=''){
+          ruDiction = JSON.parse(ruDiction);
+          reperc.ruLM = JSON.parse(ruLM); reperc.avdi++;
+        }        
+        if(typeof daDiction !== 'undefined' && daDiction!=''){
+          daDiction = JSON.parse(daDiction);
+          reperc.daLM = JSON.parse(daLM); reperc.avdi++;
+        }        
+        if(typeof fiDiction !== 'undefined' && fiDiction!=''){
+          fiDiction = JSON.parse(fiDiction);
+          reperc.fiLM = JSON.parse(fiLM); reperc.avdi++;
+        }
+        
+        storyline.snd = {};
+        if(typeof svSND !== 'undefined' && svSND.length){            
+            storyline.snd.sv = {};
+            storyline.snd.sv.p = storyline.sndURL+svSND[0];
+            svSND.splice(0,1);
+            storyline.snd.sv.t = svSND;
+        }
+        if(typeof noSND !== 'undefined' && noSND.length){            
+            storyline.snd.no = {};
+            storyline.snd.no.p = storyline.sndURL+noSND[0];
+            noSND.splice(0,1);
+            storyline.snd.no.t = noSND;
+        }
+        if(typeof daSND !== 'undefined' && daSND.length){            
+            storyline.snd.da = {};
+            storyline.snd.da.p = storyline.sndURL+daSND[0];
+            daSND.splice(0,1);
+            storyline.snd.da.t = daSND;
+        }
+        if(typeof fiSND !== 'undefined' && fiSND.length){            
+            storyline.snd.fi = {};
+            storyline.snd.fi.p = storyline.sndURL+fiSND[0];
+            fiSND.splice(0,1);
+            storyline.snd.fi.t = fiSND;
+        }
+        if(typeof ruSND !== 'undefined' && ruSND.length){            
+            storyline.snd.ru = {};
+            storyline.snd.ru.p = storyline.sndURL+ruSND[0];
+            ruSND.splice(0,1);
+            storyline.snd.ru.t = ruSND;
+        }
+    },
+    initStory:function(){
+        reperc.initBookmark();
+        reperc.initDictionary();
+        reperc.storyvidx = [];
+        for(var i=0;i<reperc.storyvis.length;i++){
+            reperc.storyvidx[reperc.storyvidx.length] = i;
+        }
+        textSt = textSt.split('<muoborder>');
+        for(var i=0;i<textSt.length;i++){
+            textSt[i] = textSt[i].trim();
+            if(textSt[i] == '') continue;
+            reperc.storylines[i] = [];
+            textSt[i] = textSt[i].split('\n');
+            for(var ix=0;ix<textSt[i].length;ix++){
+                textSt[i][ix] = textSt[i][ix].trim();
+                if(textSt[i][ix] == '') continue;
+                reperc.storylines[i][reperc.storylines[i].length] = textSt[i][ix];
+            }
+        }
+	},
+    nextHdlr:function(x){
+      if(x%ImgRo == 0){
+          var stvidx = jovuniverse.getrand(0,reperc.storyvidx.length);
+          var stvi = reperc.storyvidx[stvidx];
+          storyline.chapters[storyline.current_chap].elements.narration.illustration = EHIMGURL+reperc.storyvis[stvi];
+          reperc.storyvidx.splice(stvidx,1);
+          if(!reperc.storyvidx.length){
+              for(var i=0;i<reperc.storyvis.length;i++){
+                  reperc.storyvidx[reperc.storyvidx.length] = i;
+              }
+          }
+          reperc.setBookmark(x);
+      }
+    },
+    rp:function(){
+        storyline.current_chap = reperc.starter;
+        if(reperc.starterParagraph !== false){
+          storyline.unfold_ = true;
+          storyline.chapters[storyline.current_chap].unfoldx(reperc.starterParagraph);
+        }
+        else {
+          storyline.chapters[storyline.current_chap].unfold();
+        }
+
+        
+        var stvidx = jovuniverse.getrand(0,reperc.storyvidx.length);
+        var stvi = reperc.storyvidx[stvidx];
+        storyline.chapters[storyline.current_chap].elements.narration.illustration = EHIMGURL+reperc.storyvis[stvi];
+        reperc.storyvidx.splice(stvidx,1);
+
+        document.addEventListener('unfoldx', e => {
+            reperc.nextHdlr(e.detail);
+        });
+    },
+    xDict:function(p){
+        $('body').append('<div id="dictionary" style="top:'+(p.top-40)+'px;left:'+p.left+'px;">Not Available</div>').addClass('dictionInq');
+    },
+    wo:function(r){
+      window.open(r, '_blank');
+    }
+};
+var chidx;
+var n;
+
+reperc.starter = 1;
+config = {type:'repercussion',effect:new repercussion(reperc.rp),prerequisite:false};
+chidx = storyline.addChapter(config);
+
+reperc.initStory();
+config = {type:'narration',narration:false};
+n = new narration();
+for(var i=0;i<reperc.storylines[1].length;i++){
+	//en,ru,sv,da,no,fi
+	n.addNarration(reperc.storylines[0][i],reperc.storylines[4][i],reperc.storylines[1][i],reperc.storylines[3][i],reperc.storylines[2][i],reperc.storylines[5][i]);
+}
+n.illustration = EHIMGURL+oImg;
+config.narration = n;
+chidx = storyline.addChapter(config);
+
+storyline.dictionfn = function(t,l){
+    $('#dictionary').remove();
+    $('.txtfrg.inq').removeClass('inq');
+    const pos = $(l).offset();
+    $(l).addClass('inq');
+    if(!reperc.avdi){reperc.xDict(pos);return;}
+    
+    t = t.toLowerCase().replace(/^[\p{P}\p{S}\s]+|[\p{P}\p{S}\s]+$/gu, '');
+    t = t +' - ';
+    let xx = t[0].toLowerCase();
+    
+    if(storyline.clang == 'sv'){
+        if(typeof reperc.svLM === 'undefined'){reperc.xDict(pos);return;}
+        if (!reperc.svLM[xx]){
+            reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+            return;
+        }
+        for(var i=reperc.svLM[xx].start;i<=reperc.svLM[xx].end;i++){
+            if(svDiction[i].indexOf(t) === 0){
+                $('body').append('<div id="dictionary" style="top:'+(pos.top-40)+'px;left:'+pos.left+'px;">'+svDiction[i].split(' - ')[1]+'</div>').addClass('dictionInq');
+                return;
+            }
+        }
+        reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+    }
+    else if(storyline.clang == 'no'){
+        if(typeof reperc.noLM === 'undefined'){reperc.xDict(pos);return;}
+        if (!reperc.noLM[xx]){
+            reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+            return;
+        }
+        for(var i=reperc.noLM[xx].start;i<=reperc.noLM[xx].end;i++){
+            if(noDiction[i].indexOf(t) === 0){
+                $('body').append('<div id="dictionary" style="top:'+(pos.top-40)+'px;left:'+pos.left+'px;">'+noDiction[i].split(' - ')[1]+'</div>').addClass('dictionInq');
+                return;
+            }
+        }
+        reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+    }
+    else if(storyline.clang == 'da'){
+        if(typeof reperc.daLM === 'undefined'){reperc.xDict(pos);return;}
+        if (!reperc.daLM[xx]){
+            reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+            return;
+        }
+        for(var i=reperc.daLM[xx].start;i<=reperc.daLM[xx].end;i++){
+            if(daDiction[i].indexOf(t) === 0){
+                $('body').append('<div id="dictionary" style="top:'+(pos.top-40)+'px;left:'+pos.left+'px;">'+daDiction[i].split(' - ')[1]+'</div>').addClass('dictionInq');
+                return;
+            }
+        }
+        reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+    }
+    else if(storyline.clang == 'ru'){
+        if(typeof reperc.ruLM === 'undefined'){reperc.xDict(pos);return;}
+        if (!reperc.ruLM[xx]){
+            reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+            return;
+        }
+        for(var i=reperc.ruLM[xx].start;i<=reperc.ruLM[xx].end;i++){
+            if(ruDiction[i].indexOf(t) === 0){
+                $('body').append('<div id="dictionary" style="top:'+(pos.top-40)+'px;left:'+pos.left+'px;">'+ruDiction[i].split(' - ')[1]+'</div>').addClass('dictionInq');
+                return;
+            }
+        }
+        reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+    }
+    else if(storyline.clang == 'fi'){
+        if(typeof reperc.fiLM === 'undefined'){reperc.xDict(pos);return;}
+        if (!reperc.fiLM[xx]){
+            reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+            return;
+        }
+        for(var i=reperc.fiLM[xx].start;i<=reperc.fiLM[xx].end;i++){
+            if(fiDiction[i].indexOf(t) === 0){
+                $('body').append('<div id="dictionary" style="top:'+(pos.top-40)+'px;left:'+pos.left+'px;">'+fiDiction[i].split(' - ')[1]+'</div>').addClass('dictionInq');
+                return;
+            }
+        }
+        reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+    }    
+};
+$('#text5').prop('src',EHIMGURL+oImg);
+
+
+var cssa,cssb,cssc;        
+cssa = document.createElement('style');
+cssa.type = "text/css";
+document.getElementsByTagName("BODY")[0].appendChild(cssa);
+cssb = cssa.sheet;
+cssc = '#dialoguebubble{min-height: 160px;}';
+cssb.insertRule(cssc, cssb.cssRules.length);
