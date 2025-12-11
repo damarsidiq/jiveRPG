fa6a22d0991af59a6ad138bdfaa48d5508bde55e
Erwanto Damarsidiq <45053494+damarsidiq@users.noreply.github.com>
2025-12-12 04:57:53 +0700

    Add files via upload

---
+++ Watchers.js
@@ -0,0 +1,390 @@
+var textSt = `Absolutely, both songs feature a kind of watcher, but they’re very different flavors of watcher.
+In Every Breath You Take, the lyrical voice is famously ambiguous.
+To casual listeners it feels like a romantic vow, but Sting himself has explained that the narrative voice is obsessive and controlling.
+Almost like a stalker watching every move of a lost lover.
+The repetition and focus on monitoring (every breath, every move) is part of what makes the song feel unsettling once you tune into it that way.
+By contrast, From a Distance uses the idea of watching from afar literally and almost cosmically.
+The watcher in the song’s narrative is more like an omniscient perspective on humanity.
+It’s more about seeing the big picture, a kind of empathetic surveillance rather than possessive observation.
+Yes, in contrast we can expand the concepts of fixation and release, obsession and trust using this 2 songs' lyrics.
+by being distant we get harmony and abundance.
+colorful sceneries - blue, green and white mountains.
+everyone would have enough food on their plate.
+no need for violence, no need to use guns.
+and all would be healthy as there are no diseases.
+but in a fixated state we get nightmares of seeing someone else's face.
+feeling cold, probably because of hunger.
+feeling a longing for an embrace of the person that that someone is fixated on.
+repetitive patterns of activities that feels sad and degrading.
+every single day.
+to the point of making the narrator cries.
+while the other person just plays games.
+socializing with a smile on their face and talking freely.
+by being distant we can see ourselves as a vessel or an instrument.
+gathering for a common purpose as a group, a band.
+singing songs that carries the messages of peace and hope.
+this is a beautiful dream.
+but in an obsessed state what we get is fear.
+fear of a broken agreement and lies being told by someone else.
+this is a bad dream overall.
+i just thought that this is a very interesting subject material.
+
+<muoborder>
+
+Absolut, båda låtarna har en slags observatör, men de är väldigt olika typer av observatörer.
+I Every Breath You Take är den lyriska rösten berömd för sin tvetydighet.
+För avslappnade lyssnare känns det som ett romantiskt löfte, men Sting själv har förklarat att den narrativa rösten är besatt och kontrollerande.
+Nästan som en stalker som ser varje rörelse av en förlorad älskare.
+Upprepningen och fokus på övervakning (varje andetag, varje rörelse) är en del av vad som gör att låten känns obehaglig när man lyssnar på det sättet.
+I kontrast använder From a Distance idén om att titta på avstånd bokstavligt och nästan kosmiskt.
+Observatören i låtens berättelse är mer som ett allvetande perspektiv på mänskligheten.
+Det handlar mer om att se den stora bilden, en slags empatisk övervakning snarare än besittande observation.
+Ja, i kontrast kan vi utvidga begreppen fixering och frigivning, besatthet och förtroende med hjälp av dessa två låtars texter.
+Genom att vara distanserade får vi harmoni och överflöd.
+Färgglada landskap - blå, gröna och vita berg.
+Alla skulle ha tillräckligt med mat på sin tallrik.
+Ingen anledning till våld, inget behov av att använda vapen.
+Och alla skulle vara friska eftersom det inte finns några sjukdomar.
+Men i ett fixerat tillstånd får vi mardrömmar av att se någon annans ansikte.
+Känna sig kall, förmodligen på grund av hunger.
+Känna en längtan efter en omfamning från den person som någon är fixerad vid.
+Repetitiva mönster av aktiviteter som känns sorgliga och nedbrytande.
+Varje enda dag.
+Till den grad att få berättaren att gråta.
+Medan den andra personen bara spelar spel.
+Socialiserar med ett leende på ansiktet och pratar fritt.
+Genom att vara distanserade kan vi se oss själva som ett kärl eller ett instrument.
+Samlas för ett gemensamt syfte som en grupp, ett band.
+Sjunger låtar som bär budskap om fred och hopp.
+Detta är en vacker dröm.
+Men i ett besatt tillstånd får vi rädsla.
+Rädsla för ett brutet avtal och lögner som berättas av någon annan.
+Detta är en dålig dröm överlag.
+Jag tyckte bara att detta är ett väldigt intressant ämne.
+
+
+<muoborder>
+
+Абсолютно, обе песни содержат образ наблюдателя, но это очень разные виды наблюдателя.
+В "Every Breath You Take" лирический голос знаменит своей неоднозначностью.
+Для случайных слушателей это звучит как романтическая клятва, но сам Стинг объяснил, что повествовательный голос одержим и контролирующий.
+Почти как сталкер, следящий за каждым движением потерянной любви.
+Повторение и акцент на наблюдении (каждый вдох, каждое движение) — это часть того, что делает песню тревожной, когда вы воспринимаете её именно так.
+В отличие от этого, "From a Distance" использует идею наблюдения издалека буквально и почти космически.
+Наблюдатель в нарративе песни больше похож на всеведущую перспективу на человечество.
+Это больше о том, чтобы видеть общую картину, своего рода эмпатическое наблюдение, а не собственническое.
+Да, в контрасте мы можем расширить концепции фиксации и освобождения, одержимости и доверия, используя тексты этих двух песен.
+Будучи на расстоянии, мы получаем гармонию и изобилие.
+Красочные пейзажи - синие, зеленые и белые горы.
+У каждого будет достаточно еды на тарелке.
+Нет нужды в насилии, нет нужды использовать оружие.
+И все будут здоровы, так как болезней не будет.
+Но в состоянии фиксации мы получаем кошмары, видя лицо другого человека.
+Чувствуя холод, вероятно, из-за голода.
+Чувствуя тоску по объятиям того человека, на котором кто-то зациклен.
+Повторяющиеся паттерны действий, которые кажутся грустными и уничижительными.
+Каждый божий день.
+До такой степени, что заставляют рассказчика плакать.
+В то время как другой человек просто играет в игры.
+Общается с улыбкой на лице и говорит свободно.
+Будучи на расстоянии, мы можем видеть себя как сосуд или инструмент.
+Собираясь для общей цели как группа, как группа.
+Поем песни, которые несут послания мира и надежды.
+Это прекрасная мечта.
+Но в состоянии одержимости то, что мы получаем, — это страх.
+Страх сломанного соглашения и лжи, сказанной кем-то другим.
+Это в целом плохой сон.
+Я просто подумал, что это очень интересный предмет для обсуждения.`;
+
+
+var svSND = ['distance.mp3',[0.000000,6.540011,0],[6.977178,11.925902,1],[12.433016,20.459393,2],[21.018966,25.355658,3],[25.862771,35.148188,4],[35.602841,41.390925,5],[41.985472,47.388850,6],[47.895963,54.523408,7],[55.117954,63.161818,8],[63.668931,67.778296,9],[68.337869,72.307341,10],[72.744508,76.224353,11],[76.626546,80.648478,12],[81.138105,85.247470,13],[85.614690,90.755768,14],[91.210421,94.795187,15],[95.284813,99.953752,16],[100.565785,105.234723,17],[105.776810,107.577936,18],[108.085049,111.075268,19],[111.687301,114.712493,20],[115.254580,119.119132,21],[119.591271,124.574970,22],[125.134543,128.894175,23],[129.541181,132.916107,24],[133.510653,135.626539,25],[136.116166,139.193818,26],[139.648471,144.247463,27],[144.772063,147.552442,28],[148.007095,151.679293,29]];
+
+
+/*things to adjust*/
+var EHIMGURL;
+if(storyline.gtUrl.indexOf('https:') === 0){
+    EHIMGURL = jovuniverse.EHIMGURL+'csh/';
+}
+else{
+    EHIMGURL = storyline.BIMG+'csh/';
+}
+const svisual = ['7.jpg','8.jpg','9.jpg','1a.jpg','q.jpg','o.jpg','5.jpg','1.jpg','6.jpg','3.jpg','4.jpg','2.jpg','x.jpg','z.jpg','v.jpg','u.jpg','y.jpg','t.jpg','s.jpg','w.jpg','p.jpg','b.jpg','c.jpg','a.jpg','g.jpg','f.jpg','m.jpg','k.jpg','d.jpg','l.jpg','e.jpg','h.jpg','j.jpg','i.jpg','r.jpg','n.jpg'];
+const ImgRo = 1;
+const oImg = '1a.jpg';
+const ehbmtitle = 'jgstly';
+
+
+storyline.intro = `<p>Discussion about 2 songs lyric (Julia Gold's From a Distance & Sting's Every Breath You Take). 30 slides in total.</p>
+<div style="font-size:smaller;">
+<p>Voice Available:<br>SV</p>
+<p>Story Text(line 1-9):<br><a href="https://www.chatgpt.com" target="_blank">ChatGPT</a></p>
+<p>Translations:<br><a href="https://www.easemate.ai/ai-translator" target="_blank">EaseMate AI</a></p>
+<p>Voices:<br>
+<a href="https://voicertool.com" target="_blank">VoicerTool</a>
+</p>
+<p>Images:<br><a href="https://www.pixabay.com" target="_blank">pixabay.com</a></p></div>`;
+
+storyline.lang = ['EN','SV','RU'];
+storyline.clang = storyline.deflang = 'sv';
+/*eo things to adjust*/
+
+
+var reperc = {
+    starter:false,
+    storylines:[],
+    starterParagraph:false,
+    bmidx:false,
+    storyvis:svisual,
+    setBookmark:function(x){
+        if(reperc.localstorage == false){return;}
+        reperc.bookmark[reperc.bmidx].cpar = x;
+        reperc.localstorage.setItem('southSideBM',JSON.stringify(reperc.bookmark));
+    },
+    initBookmark:function(){
+        if (typeof localStorage == "object"){
+            reperc.localstorage =  localStorage;
+        } else if (typeof globalStorage == "object"){
+            reperc.localstorage = globalStorage[location.host];
+        } else {
+            reperc.localstorage = false;
+        }
+        if(reperc.localstorage !== false){
+            reperc.bookmark = reperc.localstorage.getItem('southSideBM');
+            if(reperc.bookmark == null){
+                reperc.bookmark = [];
+                reperc.bmidx = 0;
+                reperc.bookmark[0] = {title:ehbmtitle,cpar:0};
+            }
+            else{
+                reperc.bookmark = JSON.parse(reperc.bookmark);
+                for(var i=0;i<reperc.bookmark.length;i++){
+                    if(reperc.bookmark[i].title == ehbmtitle){
+                        reperc.starterParagraph = reperc.bookmark[i].cpar;
+                        reperc.bmidx = i;
+                        break;
+                    }
+                }
+                if(reperc.bmidx === false){
+                    reperc.bmidx = reperc.bookmark.length;
+                    reperc.bookmark[reperc.bmidx] = {title:ehbmtitle,cpar:0};
+                }
+            }
+        }
+    },
+    avdi:0,
+    initDictionary:function(){
+        if(typeof svDiction !== 'undefined' && svDiction!=''){
+          svDiction = JSON.parse(svDiction);
+          reperc.svLM = JSON.parse(svLM);   reperc.avdi++;
+        }        
+        if(typeof noDiction !== 'undefined' && noDiction!=''){
+          noDiction = JSON.parse(noDiction);
+          reperc.noLM = JSON.parse(noLM); reperc.avdi++;
+        }        
+        if(typeof ruDiction !== 'undefined' && ruDiction!=''){
+          ruDiction = JSON.parse(ruDiction);
+          reperc.ruLM = JSON.parse(ruLM); reperc.avdi++;
+        }        
+        if(typeof daDiction !== 'undefined' && daDiction!=''){
+          daDiction = JSON.parse(daDiction);
+          reperc.daLM = JSON.parse(daLM); reperc.avdi++;
+        }        
+        if(typeof fiDiction !== 'undefined' && fiDiction!=''){
+          fiDiction = JSON.parse(fiDiction);
+          reperc.fiLM = JSON.parse(fiLM); reperc.avdi++;
+        }
+        
+        storyline.snd = {};
+        if(typeof svSND !== 'undefined' && svSND.length){            
+            storyline.snd.sv = {};
+            storyline.snd.sv.p = storyline.sndURL+svSND[0];
+            svSND.splice(0,1);
+            storyline.snd.sv.t = svSND;
+        }
+        if(typeof noSND !== 'undefined' && noSND.length){            
+            storyline.snd.no = {};
+            storyline.snd.no.p = storyline.sndURL+noSND[0];
+            noSND.splice(0,1);
+            storyline.snd.no.t = noSND;
+        }
+        if(typeof daSND !== 'undefined' && daSND.length){            
+            storyline.snd.da = {};
+            storyline.snd.da.p = storyline.sndURL+daSND[0];
+            daSND.splice(0,1);
+            storyline.snd.da.t = daSND;
+        }
+        if(typeof fiSND !== 'undefined' && fiSND.length){            
+            storyline.snd.fi = {};
+            storyline.snd.fi.p = storyline.sndURL+fiSND[0];
+            fiSND.splice(0,1);
+            storyline.snd.fi.t = fiSND;
+        }
+        if(typeof ruSND !== 'undefined' && ruSND.length){            
+            storyline.snd.ru = {};
+            storyline.snd.ru.p = storyline.sndURL+ruSND[0];
+            ruSND.splice(0,1);
+            storyline.snd.ru.t = ruSND;
+        }
+    },
+    initStory:function(){
+        reperc.initBookmark();
+        reperc.initDictionary();
+        reperc.storyvidx = [];
+        for(var i=0;i<reperc.storyvis.length;i++){
+            reperc.storyvidx[reperc.storyvidx.length] = i;
+        }
+        textSt = textSt.split('<muoborder>');
+        for(var i=0;i<textSt.length;i++){
+            textSt[i] = textSt[i].trim();
+            if(textSt[i] == '') continue;
+            reperc.storylines[i] = [];
+            textSt[i] = textSt[i].split('\n');
+            for(var ix=0;ix<textSt[i].length;ix++){
+                textSt[i][ix] = textSt[i][ix].trim();
+                if(textSt[i][ix] == '') continue;
+                reperc.storylines[i][reperc.storylines[i].length] = textSt[i][ix];
+            }
+        }
+	},
+    nextHdlr:function(x){
+      if(x%ImgRo == 0){
+          var stvidx = jovuniverse.getrand(0,reperc.storyvidx.length);
+          var stvi = reperc.storyvidx[stvidx];
+          storyline.chapters[storyline.current_chap].elements.narration.illustration = EHIMGURL+reperc.storyvis[stvi];
+          reperc.storyvidx.splice(stvidx,1);
+          if(!reperc.storyvidx.length){
+              for(var i=0;i<reperc.storyvis.length;i++){
+                  reperc.storyvidx[reperc.storyvidx.length] = i;
+              }
+          }
+          reperc.setBookmark(x);
+      }
+    },
+    rp:function(){
+        storyline.current_chap = reperc.starter;
+        if(reperc.starterParagraph !== false){
+          storyline.unfold_ = true;
+          storyline.chapters[storyline.current_chap].unfoldx(reperc.starterParagraph);
+        }
+        else {
+          storyline.chapters[storyline.current_chap].unfold();
+        }
+
+        
+        var stvidx = jovuniverse.getrand(0,reperc.storyvidx.length);
+        var stvi = reperc.storyvidx[stvidx];
+        storyline.chapters[storyline.current_chap].elements.narration.illustration = EHIMGURL+reperc.storyvis[stvi];
+        reperc.storyvidx.splice(stvidx,1);
+
+        document.addEventListener('unfoldx', e => {
+            reperc.nextHdlr(e.detail);
+        });
+    },
+    xDict:function(p){
+        $('body').append('<div id="dictionary" style="top:'+(p.top-40)+'px;left:'+p.left+'px;">Not Available</div>').addClass('dictionInq');
+    },
+    wo:function(r){
+      window.open(r, '_blank');
+    }
+};
+var chidx;
+var n;
+
+reperc.starter = 1;
+config = {type:'repercussion',effect:new repercussion(reperc.rp),prerequisite:false};
+chidx = storyline.addChapter(config);
+
+reperc.initStory();
+config = {type:'narration',narration:false};
+n = new narration();
+for(var i=0;i<reperc.storylines[1].length;i++){
+	//en,ru,sv,da,no,fi
+	n.addNarration(reperc.storylines[0][i],reperc.storylines[2][i],reperc.storylines[1][i],null,null,null);
+}
+n.illustration = EHIMGURL+oImg;
+config.narration = n;
+chidx = storyline.addChapter(config);
+
+storyline.dictionfn = function(t,l){
+    $('#dictionary').remove();
+    $('.txtfrg.inq').removeClass('inq');
+    const pos = $(l).offset();
+    $(l).addClass('inq');
+    if(!reperc.avdi){reperc.xDict(pos);return;}
+    
+    t = t.toLowerCase().replace(/^[\p{P}\p{S}\s]+|[\p{P}\p{S}\s]+$/gu, '');
+    t = t +' - ';
+    let xx = t[0].toLowerCase();
+    
+    if(storyline.clang == 'sv'){
+        if(typeof reperc.svLM === 'undefined'){reperc.xDict(pos);return;}
+        if (!reperc.svLM[xx]){
+            reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+            return;
+        }
+        for(var i=reperc.svLM[xx].start;i<=reperc.svLM[xx].end;i++){
+            if(svDiction[i].indexOf(t) === 0){
+                $('body').append('<div id="dictionary" style="top:'+(pos.top-40)+'px;left:'+pos.left+'px;">'+svDiction[i].split(' - ')[1]+'</div>').addClass('dictionInq');
+                return;
+            }
+        }
+        reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+    }
+    else if(storyline.clang == 'no'){
+        if(typeof reperc.noLM === 'undefined'){reperc.xDict(pos);return;}
+        if (!reperc.noLM[xx]){
+            reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+            return;
+        }
+        for(var i=reperc.noLM[xx].start;i<=reperc.noLM[xx].end;i++){
+            if(noDiction[i].indexOf(t) === 0){
+                $('body').append('<div id="dictionary" style="top:'+(pos.top-40)+'px;left:'+pos.left+'px;">'+noDiction[i].split(' - ')[1]+'</div>').addClass('dictionInq');
+                return;
+            }
+        }
+        reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+    }
+    else if(storyline.clang == 'da'){
+        if(typeof reperc.daLM === 'undefined'){reperc.xDict(pos);return;}
+        if (!reperc.daLM[xx]){
+            reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+            return;
+        }
+        for(var i=reperc.daLM[xx].start;i<=reperc.daLM[xx].end;i++){
+            if(daDiction[i].indexOf(t) === 0){
+                $('body').append('<div id="dictionary" style="top:'+(pos.top-40)+'px;left:'+pos.left+'px;">'+daDiction[i].split(' - ')[1]+'</div>').addClass('dictionInq');
+                return;
+            }
+        }
+        reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+    }
+    else if(storyline.clang == 'ru'){
+        if(typeof reperc.ruLM === 'undefined'){reperc.xDict(pos);return;}
+        if (!reperc.ruLM[xx]){
+            reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+            return;
+        }
+        for(var i=reperc.ruLM[xx].start;i<=reperc.ruLM[xx].end;i++){
+            if(ruDiction[i].indexOf(t) === 0){
+                $('body').append('<div id="dictionary" style="top:'+(pos.top-40)+'px;left:'+pos.left+'px;">'+ruDiction[i].split(' - ')[1]+'</div>').addClass('dictionInq');
+                return;
+            }
+        }
+        reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+    }
+    else if(storyline.clang == 'fi'){
+        if(typeof reperc.fiLM === 'undefined'){reperc.xDict(pos);return;}
+        if (!reperc.fiLM[xx]){
+            reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+            return;
+        }
+        for(var i=reperc.fiLM[xx].start;i<=reperc.fiLM[xx].end;i++){
+            if(fiDiction[i].indexOf(t) === 0){
+                $('body').append('<div id="dictionary" style="top:'+(pos.top-40)+'px;left:'+pos.left+'px;">'+fiDiction[i].split(' - ')[1]+'</div>').addClass('dictionInq');
+                return;
+            }
+        }
+        reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');
+    }    
+};
+$('#text5').prop('src',EHIMGURL+oImg);
