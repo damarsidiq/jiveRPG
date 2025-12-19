1d47e9755f632c7869455e042021c3f2b9859249
Erwanto Damarsidiq <45053494+damarsidiq@users.noreply.github.com>
2025-12-20 02:20:32 +0700

    Add files via upload

---
+++ Įvadas.js
@@ -0,0 +1,345 @@
+var textSt = `my house is small but beautiful.
+i would like to have a big and beautiful house.
+but i dont think i need that right now.
+right now i am single, i am not married.
+but i would like to someday.
+with a beautiful woman.
+i dont have a lot of money right now.
+i only have some amount of money.
+but maybe i can be a rich man in the future.
+i work every day.
+seven days in one week.
+about eight hours in one day.
+i have one computer and two good hands.
+my favorite color is blue
+but i also like red, orange, yellow, green and purple.
+i like reading articles.
+but its been a while since i read a book.
+i love learning foreign languages.
+spanish, german, french, italian, swedish, norwegian,danish, russian, japanese.
+i drink coffee, milk, tea, juice and water.
+i dont drink wine or beer.
+i eat rice, eggs, chicken, meat.
+i dont eat pigs
+i sometimes write stories.
+i would like to write a book someday.
+i go to bed everynight and exercise in the morning.
+i usually sleep for about five to six hours.
+but when i am tired i can sleep for seven,eight,nine,ten,eleven or twelve hours.
+i usually wake up very early in the morning.
+i like watching movies, soccer and also basketball.
+i also like watching podcasts on youtube and also the news.
+i like walking and running and doing yoga.
+i dont like boxing or fighting sports.
+i dont have a car, bicycle or motorcycle.
+i know how to ride a bicycle and motorcycle.
+but i cant drive a car.
+i am 35 years old.
+my name is...
+
+<muoborder>
+
+Mano namas mažas, bet gražus.  
+Norėčiau turėti didelį ir gražų namą.  
+Bet nemanau, kad man to dabar reikia.  
+Dabar esu vienišas, nesu vedęs.  
+Bet norėčiau kada nors.  
+Su gražia moterimi.  
+Dabar neturiu daug pinigų.  
+Turiu tik tam tikrą pinigų sumą.  
+Bet galbūt ateityje galiu tapti turtingu žmogumi.  
+Dirbu kiekvieną dieną.  
+Septynios dienos per savaitę.  
+Apie aštuonias valandas per dieną.  
+Turiu vieną kompiuterį ir dvi geras rankas.  
+Mano mėgstamiausia spalva yra mėlyna,  
+bet man taip pat patinka raudona, oranžinė, geltona, žalia ir violetinė.  
+Man patinka skaityti straipsnius.  
+Bet jau praėjo šiek tiek laiko, kai skaičiau knygą.  
+Myliu mokytis užsienio kalbų.  
+Ispanų, vokiečių, prancūzų, italų, švedų, norvegų, danų, rusų, japonų.  
+Gerkiau kavą, pieną, arbatą, sultis ir vandenį.  
+Negeriu vyno ar alaus.  
+Valgau ryžius, kiaušinius, vištieną, mėsą.  
+Negaliu valgyti kiaulių.  
+Kartais rašau istorijas.  
+Norėčiau kada nors parašyti knygą.  
+Kiekvieną naktį einu miegoti ir ryte mankštinuosi.  
+Paprastai miegu apie penkias ar šešias valandas.  
+Bet kai esu pavargęs, galiu miegoti septynias, aštuonias, devynias, dešimt, vienuolika ar dvylika valandų.  
+Paprastai pabundu labai anksti ryte.  
+Man patinka žiūrėti filmus, futbolą ir taip pat krepšinį.  
+Man taip pat patinka žiūrėti podkastus „YouTube“ ir taip pat naujienas.  
+Man patinka vaikščioti, bėgioti ir daryti jogą.  
+Man nepatinka bokso ar kovų sportai.  
+Neturiu automobilio, dviračio ar motociklo.  
+Moku važiuoti dviračiu ir motociklu.  
+Bet negaliu vairuoti automobilio.  
+Man 35 metai.  
+Mano vardas yra...
+
+<muoborder>
+A házam kicsi, de szép.  
+Szeretnék egy nagy és szép házat.  
+De nem hiszem, hogy most szükségem van rá.  
+Jelenleg egyedülálló vagyok, nem vagyok házas.  
+De szeretnék egyszer.  
+Egy szép nővel.  
+Jelenleg nincs sok pénzem.  
+Csak egy bizonyos összegű pénzem van.  
+De talán a jövőben gazdag ember lehetek.  
+Minden nap dolgozom.  
+Hét nap egy héten.  
+Körülbelül nyolc órát egy nap.  
+Van egy számítógépem és két jó kezem.  
+A kedvenc színem a kék.  
+De szeretem a pirosat, narancssárgát, sárgát, zöldet és lilát is.  
+Szeretek cikkeket olvasni.  
+De már egy ideje nem olvastam könyvet.  
+Szeretem a külföldi nyelvek tanulását.  
+Spanyol, német, francia, olasz, svéd, norvég, dán, orosz, japán.  
+Kávét, tejet, teát, gyümölcslevet és vizet iszom.  
+Nem iszom bort vagy sört.  
+Rizst, tojást, csirkét, húst eszem.  
+Nem eszem disznót.  
+Néha írok történeteket.  
+Szeretnék egyszer könyvet írni.  
+Minden este lefekszem, és reggel edzek.  
+Általában körülbelül öt-hat órát alszom.  
+De amikor fáradt vagyok, tudok aludni hét, nyolc, kilenc, tíz, tizenegy vagy tizenkét órát is.  
+Általában nagyon korán kelek reggel.  
+Szeretek filmet nézni, focizni és kosárlabdázni is.  
+Szeretek podcasteket nézni a YouTube-on és a híreket is.  
+Szeretek sétálni, futni és jógázni.  
+Nem szeretem a bokszot vagy a küzdősportokat.  
+Nincs autóm, biciklim vagy motorom.  
+Tudok biciklizni és motorozni.  
+De nem tudok autót vezetni.  
+35 éves vagyok.  
+A nevem...
+
+<muoborder>
+Minu maja on väike, aga ilus.  
+Ma tahaksin omada suurt ja ilusat maja.  
+Aga ma ei arva, et mul on seda praegu vaja.  
+Praegu olen ma vallaline, ma ei ole abielus.  
+Aga ma tahaksin kunagi.  
+Ilusa naisega.  
+Mul ei ole praegu palju raha.  
+Mul on ainult mõni summa raha.  
+Aga võib-olla saan ma tulevikus rikkaks meheks.  
+Ma töötan iga päev.  
+Seitsme päeva jooksul nädalas.  
+Umbes kaheksa tundi päevas.  
+Mul on üks arvuti ja kaks head kätt.  
+Minu lemmikvärv on sinine.  
+Aga mulle meeldivad ka punane, oranž, kollane, roheline ja lilla.  
+Mulle meeldib lugeda artikleid.  
+Aga on möödunud aega, mil ma lugesin raamatut.  
+Ma armastan õppida võõrkeeli.  
+Hispaania, saksa, prantsuse, itaalia, rootsi, norra, taani, vene, jaapani.  
+Ma joon kohvi, piima, teed, mahla ja vett.  
+Ma ei joo veini ega õlut.  
+Ma söön riisi, mune, kana, liha.  
+Ma ei söö sigu.  
+Mõnikord kirjutan lugusid.  
+Ma tahaksin kunagi kirjutada raamatu.  
+Ma lähen igal õhtul magama ja teen hommikul trenni.  
+Ma tavaliselt magan umbes viis kuni kuus tundi.  
+Aga kui ma olen väsinud, võin magada seitse, kaheksa, üheksa, kümme, üksteist või kaksteist tundi.  
+Ma tavaliselt ärkan väga vara hommikul.  
+Mulle meeldib vaadata filme, jalgpalli ja ka korvpalli.  
+Mulle meeldivad ka podcastide vaatamine YouTube'is ja ka uudised.  
+Mulle meeldib jalutada ja joosta ning teha joogat.  
+Ma ei meeldi poks ega võitlusspordialad.  
+Mul ei ole autot, jalgratast ega mootorratast.  
+Ma oskan sõita jalgrattaga ja mootorrattaga.  
+Aga ma ei oska autot juhtida.  
+Ma olen 35 aastat vana.  
+Minu nimi on...
+
+<muoborder>
+Mans māja ir maza, bet skaista.  
+Es vēlētos, lai man būtu liela un skaista māja.  
+Bet es nedomāju, ka man tas šobrīd ir nepieciešams.  
+Šobrīd esmu viens, neesmu precējies.  
+Bet es vēlētos kādu dienu.  
+Ar skaistu sievieti.  
+Man šobrīd nav daudz naudas.  
+Man ir tikai kāda naudas summa.  
+Bet varbūt nākotnē varu kļūt par bagātu vīrieti.  
+Es strādāju katru dienu.  
+Septiņas dienas nedēļā.  
+Aptuveni astoņas stundas vienā dienā.  
+Man ir viens dators un divas labas rokas.  
+Mana mīļākā krāsa ir zila.  
+Bet man arī patīk sarkana, oranža, dzeltena, zaļa un violeta.  
+Man patīk lasīt rakstus.  
+Bet ir pagājis laiks, kopš esmu lasījis grāmatu.  
+Es mīlu mācīties svešvalodas.  
+Spāņu, vācu, franču, itāļu, zviedru, norvēģu, dāņu, krievu, japāņu.  
+Es dzeru kafiju, pienu, tēju, sulu un ūdeni.  
+Es nedzeru vīnu vai alu.  
+Es ēdu rīsus, olas, vistu, gaļu.  
+Es neēdu cūkas.  
+Es dažreiz rakstu stāstus.  
+Es vēlētos kādu dienu uzrakstīt grāmatu.  
+Es eju gulēt katru nakti un vingroju no rīta.  
+Parasti es guļu apmēram piecas līdz sešas stundas.  
+Bet, kad esmu noguris, varu gulēt septiņas, astoņas, deviņas, desmit, vienpadsmit vai divpadsmit stundas.  
+Es parasti mostos ļoti agri no rīta.  
+Man patīk skatīties filmas, futbolu un arī basketbolu.  
+Man arī patīk skatīties podkāstus youtube un arī ziņas.  
+Man patīk staigāt, skriet un nodarboties ar jogu.  
+Man nepatīk bokss vai cīņas sporta veidi.  
+Man nav automašīnas, velosipēda vai motocikla.  
+Es zinu, kā braukt ar velosipēdu un motociklu.  
+Bet es nevaru vadīt automašīnu.  
+Man ir 35 gadi.  
+Mans vārds ir...
+
+<muoborder>
+мій дім маленький, але красивий.  
+я хотів би мати великий і красивий дім.  
+але я не думаю, що мені це потрібно зараз.  
+зараз я самотній, я не одружений.  
+але я хотів би колись.  
+з красивою жінкою.  
+зараз у мене не багато грошей.  
+у мене є лише певна сума грошей.  
+але, можливо, я зможу стати багатим чоловіком у майбутньому.  
+я працюю кожен день.  
+сім днів на тиждень.  
+приблизно вісім годин на день.  
+у мене є один комп'ютер і дві добрі руки.  
+мій улюблений колір - синій,  
+але мені також подобається червоний, оранжевий, жовтий, зелений і фіолетовий.  
+мені подобається читати статті.  
+але вже давно я не читав книгу.  
+я люблю вивчати іноземні мови.  
+іспанську, німецьку, французьку, італійську, шведську, норвезьку, данську, російську, японську.  
+я п'ю каву, молоко, чай, сік і воду.  
+я не п'ю вино чи пиво.  
+я їм рис, яйця, курку, м'ясо.  
+я не їм свиней.  
+іноді я пишу історії.  
+я хотів би колись написати книгу.  
+я лягаю спати щоночі і займаюся спортом вранці.  
+зазвичай я сплю приблизно п'ять-шість годин.  
+але коли я втомлений, я можу спати сім, вісім, дев'ять, десять, одинадцять або дванадцять годин.  
+я зазвичай прокидаюся дуже рано вранці.  
+мені подобається дивитися фільми, футбол і також баскетбол.  
+мені також подобається дивитися подкасти на YouTube і також новини.  
+мені подобається гуляти, бігати і займатися йогою.  
+мені не подобається бокс чи бойові види спорту.  
+у мене немає автомобіля, велосипеда чи мотоцикла.  
+я вмію їздити на велосипеді та мотоциклі.  
+але я не можу водити автомобіль.  
+мені 35 років.  
+мене звати...
+
+
+<muoborder>
+мой дом маленькі, але прыгожы.  
+я хацеў бы мець вялікі і прыгожы дом.  
+але я не думаю, што мне гэта трэба зараз.  
+зараз я адзіночка, я не жанаты.  
+але я хацеў бы некалі.  
+з прыгожай жанчынай.  
+зараз у мяне не шмат грошай.  
+у мяне толькі пэўная колькасць грошай.  
+але, магчыма, я змагу стаць багатым чалавекам у будучыні.  
+я працую кожны дзень.  
+сем дзён на тыдні.  
+прыкладна восем гадзін за дзень.  
+у мяне ёсць адзін камп'ютар і дзве добрыя рукі.  
+мой любімы колер - сіні.  
+але мне таксама падабаецца чырвоны, аранжавы, жоўты, зялёны і фіялетавы.  
+мне падабаецца чытаць артыкулы.  
+але прайшло ўжо некаторы час з таго часу, як я чытаў кнігу.  
+я люблю вывучаць замежныя мовы.  
+іспанскую, нямецкую, французскую, італьянскую, шведскую, нарвежскую, датскую, рускую, японскую.  
+я п'ю каву, малако, чай, сок і ваду.  
+я не п'ю віно або піва.  
+я ем рыс, яйкі, курыцу, мяса.  
+я не ем свінняў.  
+я часам пішу гісторыі.  
+я хацеў бы некалі напісаць кнігу.  
+я кладуся спаць кожную ноч і займаюся спортам раніцай.  
+я звычайна сплю каля пяці-шасці гадзін.  
+але калі я стомлены, я магу спаць сем, восем, дзевяць, дзесяць, адзінаццаць або дванаццаць гадзін.  
+я звычайна прачынаюся вельмі рана раніцай.  
+мне падабаецца глядзець фільмы, футбол і таксама баскетбол.  
+мне таксама падабаецца глядзець падкасты на youtube і таксама навіны.  
+мне падабаецца хадзіць, бегаць і займацца ёгай.  
+я не люблю бокс або баявыя спорты.  
+у мяне няма аўтамабіля, веласіпеда або матацыкла.  
+я ведаю, як ехаць на веласіпедзе і матацыкле.  
+але я не магу кіраваць аўтамабілем.  
+мне 35 гадоў.  
+маё імя...
+<muoborder>
+Mój dom jest mały, ale piękny.  
+Chciałbym mieć duży i piękny dom.  
+Ale nie sądzę, żebym tego potrzebował w tej chwili.  
+W tej chwili jestem singlem, nie jestem żonaty.  
+Ale chciałbym kiedyś.  
+Z piękną kobietą.  
+Nie mam teraz dużo pieniędzy.  
+Mam tylko pewną ilość pieniędzy.  
+Ale może w przyszłości mogę być bogatym człowiekiem.  
+Pracuję każdego dnia.  
+Siedem dni w tygodniu.  
+Około ośmiu godzin dziennie.  
+Mam jeden komputer i dwie dobre ręce.  
+Mój ulubiony kolor to niebieski,  
+ale lubię też czerwony, pomarańczowy, żółty, zielony i fioletowy.  
+Lubię czytać artykuły.  
+Ale minęło trochę czasu, odkąd czytałem książkę.  
+Uwielbiam uczyć się języków obcych.  
+Hiszpański, niemiecki, francuski, włoski, szwedzki, norweski, duński, rosyjski, japoński.  
+Piję kawę, mleko, herbatę, sok i wodę.  
+Nie piję wina ani piwa.  
+Jem ryż, jajka, kurczaka, mięso.  
+Nie jem wieprzowiny.  
+Czasami piszę opowiadania.  
+Chciałbym kiedyś napisać książkę.  
+Kładę się spać każdej nocy i ćwiczę rano.  
+Zazwyczaj śpię około pięciu do sześciu godzin.  
+Ale kiedy jestem zmęczony, mogę spać przez siedem, osiem, dziewięć, dziesięć, jedenaście lub dwanaście godzin.  
+Zazwyczaj budzę się bardzo wcześnie rano.  
+Lubię oglądać filmy, piłkę nożną i także koszykówkę.  
+Lubię też oglądać podcasty na YouTube i także wiadomości.  
+Lubię chodzić, biegać i robić jogę.  
+Nie lubię boksu ani sportów walki.  
+Nie mam samochodu, roweru ani motocykla.  
+Potrafię jeździć na rowerze i motocyklu.  
+Ale nie potrafię prowadzić samochodu.  
+Mam 35 lat.  
+Nazywam się...
+`;
+
+
+/*things to adjust*/
+var EHIMGURL;
+if(storyline.gtUrl.indexOf('https:') === 0){EHIMGURL = jovuniverse.EHIMGURL+'cls/';}else{EHIMGURL = storyline.BIMG+'cls/';}
+const svisual = ['q.jpg','o.jpg','5.jpg','1.jpg','6.jpg','3.jpg','4.jpg','2.jpg','x.jpg','z.jpg','v.jpg','u.jpg','y.jpg','t.jpg','s.jpg','w.jpg','p.jpg','b.jpg','c.jpg','a.jpg','g.jpg','f.jpg','m.jpg','k.jpg','d.jpg','l.jpg','e.jpg','h.jpg','j.jpg','i.jpg','r.jpg','n.jpg'];
+const ImgRo = 1;
+const oImg = 'g.jpg';
+const ehbmtitle = 'intro1';
+
+storyline.intro = `<p>Introducing myself. 38 slides in total.</p>
+<div style="font-size:smaller;">
+<p>Voice Available:<br>-</p>
+<!--<p>Voices:<br><a href="https://voicertool.com/" target="_blank">Voicertool</a></p>-->
+<p>Translations:<ul><li><a href="https://www.easemate.ai/ai-translator" target="_blank">EaseMate AI</a></li>
+<li><a href="https://grok.com/" target="_blank">Grok</a></li>
+</ul></p>
+<p>Images:<br><ul><li><a href="https://www.pixabay.com" target="_blank">pixabay.com</a></li></ul></p></div>`;
+
+storyline.lang = ['EN','LT','HU','ET','LV','UK','BE','PL'];
+storyline.clang = storyline.deflang = 'lt';
+/*eo things to adjust*/
+
+
+var reperc={starter:false,storylines:[],starterParagraph:false,bmidx:false,storyvis:svisual,setBookmark:function(x){if(reperc.localstorage==false){return}reperc.bookmark[reperc.bmidx].cpar=x;reperc.localstorage.setItem('southSideBM',JSON.stringify(reperc.bookmark))},initBookmark:function(){if(typeof localStorage=="object"){reperc.localstorage=localStorage}else if(typeof globalStorage=="object"){reperc.localstorage=globalStorage[location.host]}else{reperc.localstorage=false}if(reperc.localstorage!==false){reperc.bookmark=reperc.localstorage.getItem('southSideBM');if(reperc.bookmark==null){reperc.bookmark=[];reperc.bmidx=0;reperc.bookmark[0]={title:ehbmtitle,cpar:0}}else{reperc.bookmark=JSON.parse(reperc.bookmark);for(var i=0;i<reperc.bookmark.length;i++){if(reperc.bookmark[i].title==ehbmtitle){reperc.starterParagraph=reperc.bookmark[i].cpar;reperc.bmidx=i;break}}if(reperc.bmidx===false){reperc.bmidx=reperc.bookmark.length;reperc.bookmark[reperc.bmidx]={title:ehbmtitle,cpar:0}}}}},avdi:0,initDictionary:function(){if(typeof ltDiction!=='undefined'&&ltDiction!=''){ltDiction=JSON.parse(ltDiction);reperc.ltLM=JSON.parse(ltLM);reperc.avdi++}storyline.snd={};if(typeof ltSND!=='undefined'&&ltSND.length){storyline.snd.lt={};storyline.snd.lt.p=storyline.sndURL+ltSND[0];ltSND.splice(0,1);storyline.snd.lt.t=ltSND}},initStory:function(){reperc.initBookmark();reperc.initDictionary();reperc.storyvidx=[];for(var i=0;i<reperc.storyvis.length;i++){reperc.storyvidx[reperc.storyvidx.length]=i}textSt=textSt.split('<muoborder>');for(var i=0;i<textSt.length;i++){textSt[i]=textSt[i].trim();if(textSt[i]=='')continue;reperc.storylines[i]=[];textSt[i]=textSt[i].split('\n');for(var ix=0;ix<textSt[i].length;ix++){textSt[i][ix]=textSt[i][ix].trim();if(textSt[i][ix]=='')continue;reperc.storylines[i][reperc.storylines[i].length]=textSt[i][ix]}}},nextHdlr:function(x){if(x%ImgRo==0){var stvidx=jovuniverse.getrand(0,reperc.storyvidx.length);var stvi=reperc.storyvidx[stvidx];storyline.chapters[storyline.current_chap].elements.narration.illustration=EHIMGURL+reperc.storyvis[stvi];reperc.storyvidx.splice(stvidx,1);if(!reperc.storyvidx.length){for(var i=0;i<reperc.storyvis.length;i++){reperc.storyvidx[reperc.storyvidx.length]=i}}reperc.setBookmark(x)}},rp:function(){storyline.current_chap=reperc.starter;if(reperc.starterParagraph!==false){storyline.unfold_=true;storyline.chapters[storyline.current_chap].unfoldx(reperc.starterParagraph)}else{storyline.chapters[storyline.current_chap].unfold()}var stvidx=jovuniverse.getrand(0,reperc.storyvidx.length);var stvi=reperc.storyvidx[stvidx];storyline.chapters[storyline.current_chap].elements.narration.illustration=EHIMGURL+reperc.storyvis[stvi];reperc.storyvidx.splice(stvidx,1);document.addEventListener('unfoldx',e=>{reperc.nextHdlr(e.detail)})},xDict:function(p){$('body').append('<div id="dictionary" style="top:'+(p.top-40)+'px;left:'+p.left+'px;">Not Available</div>').addClass('dictionInq')},wo:function(r){window.open(r,'_blank')}};var chidx;var n;reperc.starter=1;config={type:'repercussion',effect:new repercussion(reperc.rp),prerequisite:false};chidx=storyline.addChapter(config);reperc.initStory();config={type:'narration',narration:false};n=new narration();n.addCustomLang(['lt','hu','et','lv','uk','be','pl']);for(var i=0;i<reperc.storylines[1].length;i++){n.addEnglish(reperc.storylines[0][i]);n.cLNarr('lt',reperc.storylines[1][i]);n.cLNarr('hu',reperc.storylines[2][i]);n.cLNarr('et',reperc.storylines[3][i]);n.cLNarr('lv',reperc.storylines[4][i]);n.cLNarr('uk',reperc.storylines[5][i]);n.cLNarr('be',reperc.storylines[6][i]);n.cLNarr('pl',reperc.storylines[7][i])}n.illustration=EHIMGURL+oImg;config.narration=n;chidx=storyline.addChapter(config);storyline.dictionfn=function(t,l){$('#dictionary').remove();$('.txtfrg.inq').removeClass('inq');const pos=$(l).offset();$(l).addClass('inq');if(!reperc.avdi){reperc.xDict(pos);return}t=t.toLowerCase().replace(/^[\p{P}\p{S}\s]+|[\p{P}\p{S}\s]+$/gu,'');t=t+' - ';let xx=t[0].toLowerCase();if(storyline.clang=='lt'){if(typeof reperc.ltLM==='undefined'){reperc.xDict(pos);return}if(!reperc.ltLM[xx]){reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');return}for(var i=reperc.ltLM[xx].start;i<=reperc.ltLM[xx].end;i++){if(ltDiction[i].indexOf(t)===0){$('body').append('<div id="dictionary" style="top:'+(pos.top-40)+'px;left:'+pos.left+'px;">'+ltDiction[i].split(' - ')[1]+'</div>').addClass('dictionInq');return}}reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate')}else if(storyline.clang=='fi'){if(typeof reperc.fiLM==='undefined'){reperc.xDict(pos);return}if(!reperc.fiLM[xx]){reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');return}for(var i=reperc.fiLM[xx].start;i<=reperc.fiLM[xx].end;i++){if(fiDiction[i].indexOf(t)===0){$('body').append('<div id="dictionary" style="top:'+(pos.top-40)+'px;left:'+pos.left+'px;">'+fiDiction[i].split(' - ')[1]+'</div>').addClass('dictionInq');return}}reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate')}};$('#text5').prop('src',EHIMGURL+oImg);var cssa,cssb,cssc;cssa=document.createElement('style');cssa.type="text/css";document.getElementsByTagName("BODY")[0].appendChild(cssa);cssb=cssa.sheet;cssc='#dialoguebubble{min-height: 210px;}';cssb.insertRule(cssc,cssb.cssRules.length);
