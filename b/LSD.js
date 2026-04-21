4b810bf23ebae4789aa7d0ff4539e0f92749ff62
Erwanto Damarsidiq <45053494+damarsidiq@users.noreply.github.com>
2026-04-21 21:40:38 +0700

    Add files via upload

---
+++ b/LSD.js
@@ -0,0 +1,98 @@
+var textSt =`So when someone takes LSD the brain activity is lowered but the perception is widen.
+The activity of the brain is actually limiting our perception through focusing and filtering those that we want to get a grip on and handle in our daily task.
+But where does the widening of the perception happens then?
+One of the great cosmic jokes: your brain works hardest to show you the least.
+Yes, under LSD and similar psychedelics, overall brain activity actually drops in key control regions.
+The default mode network, the part that maintains your identity, filters reality, runs the story of “me,” and keeps perception tidy, gets quieter.
+The brain’s manager goes on coffee break.
+But perception widens because the filters loosen.
+Your brain is not a camera. It’s a bouncer.
+Normally it says: This is relevant. This is noise. This matters. This doesn’t. This is you. This is not.
+Most of what your senses pick up never reaches awareness. 
+If it did, you’d be paralyzed. You’d drown in data. 
+You’d sit on the floor staring at dust motes having a breakdown.
+So consciousness is normally a narrow beam.
+Psychedelics reduce top-down control. 
+The prediction systems relax. 
+The brain stops enforcing its usual models of reality. 
+Sensory regions start talking to each other more freely.
+Visual cortex chats with emotion. Memory leaks into perception.
+Sound turns into color. Time loosens.
+The widening doesn’t happen because the brain is “doing more.”
+It happens because the brain is doing less gatekeeping.
+Perception expands into the spaces that were always there but usually blocked.
+Think of it like closing all the browser tabs you didn’t know were open. 
+Suddenly the system has bandwidth. Suddenly everything loads differently. Suddenly you notice how loud the fan has been all along.
+The world didn’t change. Your interface did.
+Psychedelics temporarily disable the filter.
+Meditation slowly trains you to see through it.
+And yes, daily life requires focus. You need that tunnel vision to function. 
+Try paying taxes while experiencing the infinite fractal beauty of your own hands. Society would collapse.
+So widening perception is not about adding anything.
+It’s about subtracting constraints.
+The world is already vast.
+Your brain just edits it down to something manageable.
+Sometimes it’s nice to see the director’s cut.
+Just don’t forget you still have to wash the dishes.
+<muoborder>
+Så når noen tar LSD, reduseres hjerneaktiviteten, men persepsjonen utvides.
+Hjernens aktivitet begrenser faktisk persepsjonen vår ved å fokusere og filtrere de vi ønsker å få kontroll over og håndtere i våre daglige oppgaver.
+Men hvor skjer utvidelsen av persepsjonen da?
+En av de store kosmiske vitsene: hjernen din jobber hardest for å vise deg minst mulig.
+Ja, under LSD og lignende psykedelika, faller den totale hjerneaktiviteten faktisk i viktige kontrollområder.
+Standardmodusnettverket, den delen som opprettholder identiteten din, filtrerer virkeligheten, kjører historien om «meg» og holder persepsjonen ryddig, blir stillere.
+Hjernens leder tar kaffepause.
+Men persepsjonen utvides fordi filtrene løsner.
+Hjernen din er ikke et kamera. Det er en vippestol.
+Vanligvis står det:Dette er relevant.Dette er støy.Dette betyr noe.Dette betyr ikke noe.Dette er deg.Dette er ikke det.
+Det meste av det sansene dine plukker opp når aldri bevisstheten.
+Hvis det gjorde det, ville du blitt lammet. Du ville druknet i data.
+Du ville sittet på gulvet og stirret på støvpartikler som har et sammenbrudd.
+Så bevissthet er normalt en smal stråle.
+Psykedelika reduserer ovenfra-og-ned-kontroll.
+Prediksjonssystemene slapper av.
+Hjernen slutter å håndheve sine vanlige modeller av virkeligheten.
+Sanseområder begynner å snakke med hverandre friere.
+Visuell cortex snakker med følelser. Minne lekker inn i persepsjon.
+Lyd blir til farge. Tiden går løs.
+Utvidelsen skjer ikke fordi hjernen «gjør mer».
+Det skjer fordi hjernen gjør mindre portvakt.
+Oppfatningen utvides til områdene som alltid var der, men vanligvis blokkert.
+Tenk på det som å lukke alle nettleserfanene du ikke visste var åpne.
+Plutselig har systemet båndbredde. Plutselig laster alt annerledes. Plutselig legger du merke til hvor høylytt viften har vært hele tiden.
+Verden forandret seg ikke. Grensesnittet ditt gjorde det.
+Psykedelika deaktiverer filteret midlertidig.
+Meditasjon trener deg sakte til å se gjennom det.
+Og ja, hverdagen krever fokus. Du trenger det tunnelsynet for å fungere.
+Prøv å betale skatt mens du opplever den uendelige fraktale skjønnheten i dine egne hender. Samfunnet ville kollapse.
+Så utvidet oppfatning handler ikke om å legge til noe.
+Det handler om å trekke fra begrensninger.
+Verden er allerede enorm.
+Hjernen din redigerer den bare ned til noe håndterbart.
+Noen ganger er det fint å se regissørens klipp.
+Bare ikke glem at du fortsatt må vaske opp.`;
+
+var noDiction=`["aktivitet - activity","aldri - never","alle - all / everyone","allerede - already","alltid - always","alt - everything / all","annerledes - different","av - of / from / by","bare - only / just","begrenser - limits","begrensninger - limitations","begynner - begins","betale - pay","betyr - means","bevissthet - consciousness","bevisstheten - consciousness","blir - becomes / gets","blitt - been","blokkert - blocked","båndbredde - bandwidth","cortex - cortex","da - then / when","daglige - daily","data - data","de - they / them","deaktiverer - disables","deg - you","delen - part","den - it / that / the","der - there","det - it / that","dette - this","din - your","dine - your","ditt - your","druknet - drowned","du - you (singular)","egne - own","en - a / an / one","enorm - huge","er - is / are","et - a / an / one","faktisk - actually","faller - falling","farge - color","filteret - filter","filtrene - the filters","filtrere - filters","filtrerer - filters","fint - nice","fokus - focus","fokusere - focus","forandret - changed","fordi - because","fortsatt - still","fra - from","fraktale - fractal","friere - freer","fungere - function","følelser - emotions","få - few / get","ganger - times","gjennom - through","gjorde - did","gjør - makes","glem - forget","grensesnittet - interface","gulvet - the floor (definite form of 'gulv')","går - goes / walks","handler - is about / deals with","har - have / has","hardest - hardest","hele - whole","hender - hands","historien - the story","hjerneaktiviteten - brainactivity","hjernen - brain","hjernens - brain's","holder - keeps / holds","hverandre - each","hverdagen - everyday","hvis - if","hvor - where","høylytt - loud","håndheve - enforce","håndterbart - manageable","håndtere - handles","identiteten - identity","ikke - not","inn - in / into","ja - yes","jobber - works (present tense of 'jobbe')","kaffepause - coffee break","kamera - camera","kjører - running","klipp - cut","kollapse - collapse","kontroll - control","kontrollområder - control areas","kosmiske - cosmic","krever - demand","lammet - paralyzed","laster - loading","leder - leader","legge - add","legger - lays","lekker - delicious","lignende - similar","lukke - close","lyd - sound","løs - resolve","løsner - loosens","med - with","meditasjon - meditation","meg - me","men - but","mens - while","mer - more","merke - label","meste - most","midlertidig - temporary","mindre - less; smaller","minne - memory","minst - least","modeller - models","mulig - possible","må - must","ned - down","nettleserfanene - browser tabs","noe - something","noen - some / any","normalt - normal","når - when","og - and","om - about / if / whether","områdene - areas","opp - up","oppfatning - perception","oppfatningen - perception","oppgaver - tasks","opplever - experiencing","opprettholder - maintainer","ovenfra-og-ned-kontroll - top-down control","over - over / above","persepsjon - perception","persepsjonen - theperception","plukker - picks","plutselig - suddenly, from plutselig (sudden).","portvakt - gatekeeper","prediksjonssystemene - prediction systems","prøv - try","psykedelika - psychedelics","på - on / at / in","redigerer - editing","reduserer - reduces","reduseres - be reduced","regissørens - director's","relevant - relevant","ryddig - tidy","sakte - slow / slowly, can be used as both an adjective and adverb.","samfunnet - the society","sammenbrudd - collapse","sansene - the senses","sanseområder - sensory areas","se - see","seg - himself / herself / themselves","sine - his/her/its (reflexive possessive)","sittet - sat","skatt - tax","skjer - happens","skjønnheten - beauty","slapper - relaxes","slutter - ending","smal - narrow","snakke - talk","snakker - talks / speaks","som - that / which / who","standardmodusnettverket - default mode network","stillere - quieters","stirret - stared","store - big","stråle - beam","støvpartikler - dust particles","støy - noise","står - stands","systemet - the system","så - so / thus / then","tar - takes","tenk - think (imperative)","tiden - time","til - to / for / until","totale - total","trekke - draw","trener - trains","trenger - needs","tunnelsynet - tunnel vision","uendelige - infinite","under - during","utvidelsen - expansion","utvides - expands","utvidet - extended","vanlige - ordinary / usual","vanligvis - usually","var - was","vaske - wash","ved - by / at / about","verden - world","vi - we","viften - fan","viktige - important","ville - would / want to","vippestol - rocking chair","virkeligheten - reality","vise - show","visste - knew (past tense of 'vite')","visuell - visual","vitsene - jokes","vært - been","vår - our","våre - our","ønsker - wants / wishes","å - to (infinitive marker)","åpne - open"]`;
+var noLM=`{"a":{"start":0,"end":7},"b":{"start":8,"end":19},"c":{"start":20,"end":20},"d":{"start":21,"end":36},"e":{"start":37,"end":41},"f":{"start":42,"end":60},"g":{"start":61,"end":68},"h":{"start":69,"end":86},"i":{"start":87,"end":89},"j":{"start":90,"end":91},"k":{"start":92,"end":100},"l":{"start":101,"end":111},"m":{"start":112,"end":126},"n":{"start":127,"end":132},"o":{"start":133,"end":143},"p":{"start":144,"end":152},"r":{"start":153,"end":158},"s":{"start":159,"end":186},"t":{"start":187,"end":195},"u":{"start":196,"end":200},"v":{"start":201,"end":219},"ø":{"start":220,"end":220},"å":{"start":221,"end":222}}`;
+
+
+/*things to adjust*/
+var EHIMGURL;
+if(storyline.gtUrl.indexOf('https:') === 0){EHIMGURL = jovuniverse.EHIMGURL+'eu/';}else{EHIMGURL = storyline.BIMG+'eu/';}
+const svisual = ['de_a.jpg','de_b.jpg','de_c.jpg','de_d.jpg','de_e.jpg','de_f.jpg','es_a.jpg','es_b.jpg','es_c.jpg','es_d.jpg','es_e.jpg','es_f.jpg','es_g.jpg','es_h.jpg','es_i.jpg','ews.jpg','fr_a.jpg','fr_b.jpg','fr_c.jpg','fr_d.jpg','fr_e.jpg','fr_f.jpg','fr_g.jpg','fr_h.jpg','fr_i.jpg','fr_j.jpg','fr_k.jpg','fr_l.jpg','fr_m.jpg','fr_n.jpg','gr_a.jpg','gr_b.jpg','gr_c.jpg','gr_d.jpg','gr_e.jpg','gr_f.jpg','gr_g.jpg','gr_h.jpg','gr_i.jpg','gr_j.jpg','gr_k.jpg','gr_l.jpg','gr_m.jpg','gr_n.jpg','gr_o.jpg','gr_p.jpg','gr_q.jpg','gr_r.jpg','gr_s.jpg','it_a.jpg','it_b.jpg','it_c.jpg','it_d.jpg','it_e.jpg','it_f.jpg','it_g.jpg','it_h.jpg','it_i.jpg','it_j.jpg','it_k.jpg','it_l.jpg','1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','a.jpg','b.jpg','c.jpg','d.jpg','e.jpg','f.jpg','g.jpg','h.jpg','i.jpg','j.jpg','k.jpg','l.jpg'];
+const ImgRo = 1;
+const oImg = 'it_b.jpg';
+const ehbmtitle = 'lsd';
+
+storyline.intro = `<p>Talking about the brain's work to limit our perception.</p>
+<div style="font-size:smaller;">
+<p>Total Slides:<br> 36</p>
+<p>Story Text:<br><a href="https://chatgpt.com/" target="_blank">ChatGPT</a></p>
+<p>Translations:<br><a href="https://translate.google.com/" target="_blank">Google Translate</a></p>
+<p>Images:<br><a href="https://www.pixabay.com" target="_blank">pixabay.com</a></p></div>`;
+
+storyline.lang = ['EN','NO'];
+storyline.clang = storyline.deflang = 'no';
+/*eo things to adjust*/
+
+var reperc={starter:false,storylines:[],starterParagraph:false,bmidx:false,storyvis:svisual,setBookmark:function(x){if(reperc.localstorage==false){return}reperc.bookmark[reperc.bmidx].cpar=x;reperc.localstorage.setItem('southSideBM',JSON.stringify(reperc.bookmark))},initBookmark:function(){if(typeof localStorage=="object"){reperc.localstorage=localStorage}else if(typeof globalStorage=="object"){reperc.localstorage=globalStorage[location.host]}else{reperc.localstorage=false}if(reperc.localstorage!==false){reperc.bookmark=reperc.localstorage.getItem('southSideBM');if(reperc.bookmark==null){reperc.bookmark=[];reperc.bmidx=0;reperc.bookmark[0]={title:ehbmtitle,cpar:0}}else{reperc.bookmark=JSON.parse(reperc.bookmark);for(var i=0;i<reperc.bookmark.length;i++){if(reperc.bookmark[i].title==ehbmtitle){reperc.starterParagraph=reperc.bookmark[i].cpar;reperc.bmidx=i;break}}if(reperc.bmidx===false){reperc.bmidx=reperc.bookmark.length;reperc.bookmark[reperc.bmidx]={title:ehbmtitle,cpar:0}}}}},avdi:0,initDictionary:function(){if(typeof noDiction!=='undefined'&&noDiction!=''){noDiction=JSON.parse(noDiction);reperc.noLM=JSON.parse(noLM);reperc.avdi++}storyline.snd={};if(typeof noSND!=='undefined'&&noSND.length){storyline.snd.no={};storyline.snd.no.p=storyline.sndURL+noSND[0];noSND.splice(0,1);storyline.snd.no.t=noSND}},initStory:function(){reperc.initBookmark();reperc.initDictionary();reperc.storyvidx=[];for(var i=0;i<reperc.storyvis.length;i++){reperc.storyvidx[reperc.storyvidx.length]=i}textSt=textSt.split('<muoborder>');for(var i=0;i<textSt.length;i++){textSt[i]=textSt[i].trim();if(textSt[i]=='')continue;reperc.storylines[i]=[];textSt[i]=textSt[i].split('\n');for(var ix=0;ix<textSt[i].length;ix++){textSt[i][ix]=textSt[i][ix].trim();if(textSt[i][ix]=='')continue;reperc.storylines[i][reperc.storylines[i].length]=textSt[i][ix]}}},nextHdlr:function(x){if(x%ImgRo==0){var stvidx=jovuniverse.getrand(0,reperc.storyvidx.length);var stvi=reperc.storyvidx[stvidx];storyline.chapters[storyline.current_chap].elements.narration.illustration=EHIMGURL+reperc.storyvis[stvi];reperc.storyvidx.splice(stvidx,1);if(!reperc.storyvidx.length){for(var i=0;i<reperc.storyvis.length;i++){reperc.storyvidx[reperc.storyvidx.length]=i}}reperc.setBookmark(x)}},rp:function(){storyline.current_chap=reperc.starter;if(reperc.starterParagraph!==false){storyline.unfold_=true;storyline.chapters[storyline.current_chap].unfoldx(reperc.starterParagraph)}else{storyline.chapters[storyline.current_chap].unfold()}var stvidx=jovuniverse.getrand(0,reperc.storyvidx.length);var stvi=reperc.storyvidx[stvidx];storyline.chapters[storyline.current_chap].elements.narration.illustration=EHIMGURL+reperc.storyvis[stvi];reperc.storyvidx.splice(stvidx,1);document.addEventListener('unfoldx',e=>{reperc.nextHdlr(e.detail)})},xDict:function(p){$('body').append('<div id="dictionary" style="top:'+(p.top-40)+'px;left:'+p.left+'px;">Not Available</div>').addClass('dictionInq')},wo:function(r){window.open(r,'_blank')}};var chidx;var n;reperc.starter=1;config={type:'repercussion',effect:new repercussion(reperc.rp),prerequisite:false};chidx=storyline.addChapter(config);reperc.initStory();config={type:'narration',narration:false};n=new narration();for(var i=0;i<reperc.storylines[1].length;i++){n.addNarration(reperc.storylines[0][i],null,null,null,reperc.storylines[1][i],null)}n.illustration=EHIMGURL+oImg;config.narration=n;chidx=storyline.addChapter(config);storyline.dictionfn=function(t,l){$('#dictionary').remove();$('.txtfrg.inq').removeClass('inq');const pos=$(l).offset();$(l).addClass('inq');if(!reperc.avdi){reperc.xDict(pos);return}t=t.toLowerCase().replace(/^[\p{P}\p{S}\s]+|[\p{P}\p{S}\s]+$/gu,'');t=t+' - ';let xx=t[0].toLowerCase();if(storyline.clang=='no'){if(typeof reperc.noLM==='undefined'){reperc.xDict(pos);return}if(!reperc.noLM[xx]){reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate');return}for(var i=reperc.noLM[xx].start;i<=reperc.noLM[xx].end;i++){if(noDiction[i].indexOf(t)===0){$('body').append('<div id="dictionary" style="top:'+(pos.top-40)+'px;left:'+pos.left+'px;">'+noDiction[i].split(' - ')[1]+'</div>').addClass('dictionInq');return}}reperc.wo('https://translate.google.com/?sl='+storyline.clang+'&tl=en&text='+t+'&op=translate')}};$('#text5').prop('src',EHIMGURL+oImg);
