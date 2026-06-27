cdf092c25490cadf7ac8ea28c7ebb4040b831e21
Erwanto Damarsidiq <45053494+damarsidiq@users.noreply.github.com>
2026-06-27 11:01:30 +0700

    Add files via upload

--- Shadowing/es/SouthPole3.js
+++ Shadowing/es/SouthPole3.js
@@ -29,3 +29,6 @@ Océanos más cálidos, corrientes alteradas, cambios en los patrones climático
 Por lo tanto, el aumento del nivel del mar es solo una parte del desastre. La Antártida es menos un desierto helado y más un regulador planetario.
 Almacena agua, refleja la luz solar, estabiliza el clima e influye en la circulación oceánica.
 La gente la considera un espacio vacío en el borde del mapa. En realidad, es uno de los pilares sobre los que gira el sistema global.`;
+
+var esSND=['spole3ES.mp3',[0.000000,6.644208],[6.845542,13.640749],[13.930183,20.687630],[20.883611,31.358622],[31.654333,36.945792],[37.298146,45.855063],[46.119318,51.140224],[51.398170,58.237441],[58.665288,64.780973],[64.994885,72.834552],[73.205786,80.988800],[81.240475,91.710127],[92.131663,102.028775],[102.343361,112.246753],[112.473257,114.058808],[114.247563,119.796983],[119.796983,128.504914],[128.857266,136.860502],[137.319814,141.220739],[141.440975,148.991205],[149.343566,156.038086],[156.239439,170.685533],[170.930900,181.570430],[181.570430,189.963787],[190.284713,195.953636],[196.154975,205.668265],[205.938828,212.664811],[212.985705,224.996854],[225.336628,235.214832],[235.466503,242.463052],[242.853147,252.077012]];
+storyline.snd.es = {};storyline.snd.es.p = storyline.sndURL+esSND[0];esSND.splice(0,1);storyline.snd.es.t = esSND;
--- Shadowing/es/SouthPole4.js
+++ Shadowing/es/SouthPole4.js
@@ -15,7 +15,7 @@ Leonhard Euler ayudó a desarrollar las matemáticas de los cuerpos en rotación
 El bamboleo se refiere a cómo el eje de rotación de la Tierra cambia lentamente de dirección con el tiempo.
 Piensa en una peonza. Cuando gira rápidamente, su eje se mantiene casi vertical, pero también describe lentamente un círculo.
 Ese movimiento circular lento es la precesión. La Tierra hace lo mismo.
-Su eje está inclinado unos 23,5 grados, y a lo largo de unos 26 000 años, esa inclinación describe lentamente un círculo en el espacio.
+Su eje está inclinado unos 23,5 grados, y a lo largo de unos 26000 años, esa inclinación describe lentamente un círculo en el espacio.
 Así que ahora mismo el eje norte apunta aproximadamente hacia Polaris. Pero dentro de miles de años, apuntará hacia estrellas diferentes.
 Mismo ángulo de inclinación, diferente dirección.
 ¿Por qué oscila la Tierra? Porque el Sol y la Luna ejercen una fuerza gravitatoria sobre el abultamiento ecuatorial de la Tierra.
@@ -28,3 +28,7 @@ Así pues, «bamboleo» puede significar varias cosas, pero la precesión es la 
 ¿Por qué es importante? Porque a largo plazo afecta a las estaciones, la posición de las estrellas y los ciclos climáticos.
 La astronomía antigua, la navegación e incluso los patrones de las edades de hielo están ligados a estos movimientos.
 Tu mapa parece fijo. El planeta gira silenciosamente debajo de él.`;
+
+
+var esSND=['spole4ES.mp3',[0.000000,5.418905],[5.621481,12.154554],[12.387483,16.560580],[16.864429,28.158050],[28.341633,41.072263],[41.274849,44.516053],[44.883251,54.340984],[54.549899,63.355612],[63.874708,69.939328],[70.186205,77.510583],[77.510583,84.119641],[84.119641,95.210672],[95.508222,101.186661],[101.503167,113.645078],[114.062879,120.836523],[120.836523,130.712098],[130.952676,136.979304],[136.979304,147.323321],[147.323321,156.793744],[156.793744,160.997194],[160.997194,169.809246],[170.125767,179.178381],[179.634191,184.293422],[184.496008,195.029945],[195.029945,198.182534],[198.182534,210.932153],[210.932153,217.515869],[217.515869,225.720193],[226.137973,233.721941],[233.721941,240.052438]];
+storyline.snd.es = {};storyline.snd.es.p = storyline.sndURL+esSND[0];esSND.splice(0,1);storyline.snd.es.t = esSND;
