0ea7974a6e7a3726ef8ffd024f82fe1db20ede30
Erwanto Damarsidiq <45053494+damarsidiq@users.noreply.github.com>
2026-07-13 22:43:10 +0700

    Add files via upload

--- Shadowing/es/Zoroastrianism3.js
+++ Shadowing/es/Zoroastrianism3.js
@@ -5,7 +5,7 @@ Estos cinco períodos se denominan 'Gahs' y cada uno corresponde a un momento es
 Durante cada Gah, un zoroástrico realiza el ritual 'Kushti': desatar y volver a atar su cordón sagrado mientras recita oraciones, idealmente frente a una fuente de luz (el sol durante el día o un fuego/lámpara durante la noche).
 Los cinco Gahs diarios son: 1. 'Havan Gah:' Desde el amanecer hasta el mediodía.
 2.'Rapithwin Gah:' Desde el mediodía hasta las 3:00 p.m. (la parte más calurosa del día).
-3. 'Uzerin Gah:' Desde las 3:00 p.m. hasta el atardecer.
+    3. 'Uzerin Gah:' Desde las 3:00 p.m. hasta el atardecer.
 4. 'Aiwisruthrem Gah:' Desde el atardecer hasta la medianoche.
 5. 'Ushahin Gah:' Desde la medianoche hasta el amanecer.
 Si bien los zoroastrianos devotos históricamente observaron los cinco, las exigencias del estilo de vida moderno significan que hoy en día muchos laicos realizan el ritual menos veces, generalmente al despertar, antes de las comidas o antes de acostarse.
@@ -30,3 +30,6 @@ Sin embargo, para la gente corriente en la vida diaria, la marihuana está "proh
 El arma central que tiene un ser humano para luchar contra el mal es 'Khrad' (sabiduría y razón clara).
 El uso de drogas recreativas que alteran, embotan o intoxican la mente se considera un ataque directo a la propia sabiduría.
 Nublar tu juicio te hace muy vulnerable al engaño del reino demoníaco.`;
+
+var esSND=['zoroastro3ES.mp3',[-0.340000,6.800000,1],[7.190000,18.320000,2],[18.710000,29.020000,3],[29.420000,38.250000,4],[38.670000,53.500000,5],[53.880000,59.560000,6],[59.950000,66.720000,7],[67.110000,71.880000,8],[72.330000,77.260000,9],[77.680000,82.280000,10],[82.690000,97.680000,11],[98.120000,103.580000,12],[103.990000,108.890000,13],[109.290000,116.650000,14],[117.090000,127.990000,15],[128.370000,143.910000,16],[144.280000,153.850000,17],[154.230000,161.200000,18],[161.610000,166.280000,19],[166.670000,174.970000,20],[175.370000,182.370000,21],[182.780000,195.770000,22],[196.190000,216.120000,23],[216.550000,226.320000,24],[226.700000,236.890000,25],[237.260000,249.060000,26],[249.440000,263.670000,27],[264.060000,280.040000,28],[280.630000,286.860000,29],[287.320000,294.160000,30],[294.550000,302.190000,31],[302.580000,308.060000,32]];
+storyline.snd.es = {};storyline.snd.es.p = storyline.sndURL+esSND[0];esSND.splice(0,1);storyline.snd.es.t = esSND;
