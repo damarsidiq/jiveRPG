57f3fc72fa7dfc8da7d2c5621b168d7228dfacef
Erwanto Damarsidiq <45053494+damarsidiq@users.noreply.github.com>
2026-07-15 20:57:48 +0700

    Add files via upload

--- Shadowing/es/Taormina5.js
+++ Shadowing/es/Taormina5.js
@@ -29,3 +29,6 @@ Los japoneses construyeron filosofías. Los brasileños construyeron circuitos d
 Una tradición preservada. El otro dejó que la alfombra decidiera. Ambos funcionan.
 Uno simplemente se adapta más rápido cuando alguien intenta activamente estrangularte hasta dejarlo inconsciente.
 La historia se trata menos de invención y más de quién siguió probando la presión en lugar de pulir la historia.`;
+
+var esSND=['taormina5ES.mp3',[-0.400000,5.610000,1],[6.060000,10.440000,2],[10.950000,18.490000,3],[19.010000,26.110000,4],[26.550000,37.580000,5],[37.980000,47.800000,6],[48.390000,55.490000,7],[55.910000,62.720000,8],[63.120000,74.960000,9],[75.330000,81.630000,10],[82.030000,89.970000,11],[90.370000,100.180000,12],[100.600000,108.570000,13],[108.960000,111.800000,14],[112.200000,117.040000,15],[117.430000,129.120000,16],[129.590000,135.300000,17],[135.810000,144.750000,18],[145.140000,149.070000,19],[149.450000,158.060000,20],[158.570000,162.530000,21],[162.900000,168.370000,22],[168.900000,177.210000,23],[177.730000,188.850000,24],[189.240000,197.900000,25],[198.330000,206.330000,26],[206.930000,210.900000,27],[211.440000,218.050000,28],[218.480000,224.420000,29],[224.830000,231.460000,30],[231.890000,237.850000,31]];
+storyline.snd.es = {};storyline.snd.es.p = storyline.sndURL+esSND[0];esSND.splice(0,1);storyline.snd.es.t = esSND;
