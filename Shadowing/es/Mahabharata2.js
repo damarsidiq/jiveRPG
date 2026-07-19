9831b23cc67829b6924d1e2269ee077bc3693fac
Erwanto Damarsidiq <45053494+damarsidiq@users.noreply.github.com>
2026-07-20 00:57:46 +0700

    Add files via upload

--- Shadowing/es/Mahabharata2.js
+++ Shadowing/es/Mahabharata2.js
@@ -27,3 +27,6 @@ El Mahābhārata es lo que sucede cuando la metafísica tiene que tratar con hum
 Si quieres elegancia, lee Kālidāsa. Si quieres instrucción, lee el Gītā de forma aislada.
 Si quieres entender por qué la sabiduría colapsa bajo presión y sigue siendo importante de todos modos, el Mahābhārata es tu maestro agotador, extenso e inédito.
 No es una joya. Es una cantera.`;
+
+var esSND=['mahab2ES.mp3',[-0.280000,9.450000,1],[9.810000,21.270000,2],[21.650000,33.440000,3],[33.790000,39.270000,4],[39.670000,54.660000,5],[55.020000,63.650000,6],[64.040000,73.610000,7],[74.040000,84.300000,8],[84.680000,89.540000,9],[89.920000,95.320000,10],[95.790000,103.060000,11],[103.450000,106.670000,12],[107.110000,114.700000,13],[115.130000,117.960000,14],[118.410000,126.300000,15],[126.650000,130.740000,16],[131.170000,143.960000,17],[144.350000,148.810000,18],[149.220000,155.910000,19],[156.260000,163.240000,20],[163.630000,168.680000,21],[169.050000,178.290000,22],[178.690000,181.930000,23],[182.390000,190.700000,24],[191.140000,200.520000,25],[200.920000,205.940000,26],[206.320000,213.040000,27],[213.490000,223.450000,28],[223.860000,227.560000,29]];
+storyline.snd.es = {};storyline.snd.es.p = storyline.sndURL+esSND[0];esSND.splice(0,1);storyline.snd.es.t = esSND;
--- Shadowing/es/Mahabharata3.js
+++ Shadowing/es/Mahabharata3.js
@@ -29,3 +29,6 @@ Si el Veda trata de mantener la armonía cósmica, y el Mahābhārata trata de v
 Y sí, se siente más limpio y filosófico que el resto de la epopeya porque 'tenía' que serlo.
 Es el momento en el que la tradición se detiene, mira directamente al lector y dice: así es como se vive cuando ninguna opción parece limpia.
 Molestamente efectivo. Por eso todo el mundo sigue citándolo.`;
+
+var esSND=['mahab3ES.mp3',[-0.280000,9.200000,1],[9.570000,21.620000,2],[22.000000,39.380000,3],[39.780000,46.500000,4],[46.890000,52.430000,5],[52.850000,60.690000,6],[61.120000,66.550000,7],[66.970000,78.980000,8],[79.340000,90.280000,9],[90.780000,99.480000,10],[99.860000,105.310000,11],[105.710000,113.150000,12],[113.540000,123.480000,13],[123.890000,133.020000,14],[133.430000,142.900000,15],[143.380000,152.010000,16],[152.520000,162.760000,17],[163.180000,172.540000,18],[172.890000,182.730000,19],[183.120000,190.810000,20],[191.200000,198.270000,21],[198.660000,209.110000,22],[209.490000,214.110000,23],[214.530000,223.770000,24],[224.160000,231.510000,25],[231.920000,240.020000,26],[240.460000,249.650000,27],[250.050000,262.880000,28],[263.260000,270.250000,29],[270.680000,280.170000,30],[280.570000,286.050000,31]];
+storyline.snd.es = {};storyline.snd.es.p = storyline.sndURL+esSND[0];esSND.splice(0,1);storyline.snd.es.t = esSND;
