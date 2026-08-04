baaaa79306b99981a998a472c493cb9672f066ea
Erwanto Damarsidiq <45053494+damarsidiq@users.noreply.github.com>
2026-08-04 09:20:53 +0700

    Create Bergman.js

---
+++ nwShd/es/Bergman.js
@@ -0,0 +1,18 @@
+var shdtxt=`Marta está sentada.
+Está frente al espejo.
+Se está peinando.
+Le cuesta peinarse.
+Es muy diferente a cualquier anuncio de champú en televisión.
+Tomas está sentado.
+Está en su habitación.
+Está escribiendo en un papel.
+Parece disfrutar de esta actividad.
+Su pluma baila sobre el papel.
+Las palabras fluyen sin cesar.
+De repente, alguien llama a la puerta.
+Oswald camina por la playa.
+Karin camina a su lado.
+El mar es indiferente a su presencia.
+Las olas van y vienen.
+La playa se siente vacía sin el mar a su lado.
+Incluso si Oswald y Karin estuvieran caminando por la playa.`;
