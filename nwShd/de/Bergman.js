2185dd10c06ead5dd45288d79860f48a00398ee5
Erwanto Damarsidiq <45053494+damarsidiq@users.noreply.github.com>
2026-08-04 09:22:00 +0700

    Add Bergman.js with initial text content

---
+++ nwShd/de/Bergman.js
@@ -0,0 +1,18 @@
+var shdtxt=`Marta sitzt.
+Sie steht vor dem Spiegel.
+Sie kämmt sich die Haare.
+Sie hat Schwierigkeiten, sich die Haare zu kämmen.
+Das ist ganz anders als jede Shampoo-Werbung im Fernsehen.
+Tomas sitzt.
+Er ist in seinem Zimmer.
+Er schreibt auf einem Blatt Papier.
+Er scheint diese Tätigkeit zu genießen.
+Sein Stift tanzt über das Papier.
+Die Worte fließen unaufhörlich.
+Plötzlich klopft es an der Tür.
+Oswald geht am Strand entlang.
+Karin geht neben ihm.
+Das Meer scheint ihre Anwesenheit nicht zu bemerken.
+Die Wellen kommen und gehen.
+Der Strand wirkt leer ohne das Meer. Seite.
+Selbst wenn Oswald und Karin am Strand spazieren gingen.`;
