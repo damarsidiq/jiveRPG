16784a20f9b6ad9195b8378251c3270419c13e6b
damarsidiq <damarsidiq@gmail.com>
2026-08-26 00:20:56 +0700

    dbardsnumber

--- drawboard/db.js
+++ drawboard/db.js
@@ -1,5 +1,5 @@
 export var qr = {
-    langsTotalDB:[35,0],
+    langsTotalDB:[61,0],
     langAvail:['-es','-de'],
     initPool:function(pdb){
         var tidx = qr.langAvail.indexOf(jve.foclang);
