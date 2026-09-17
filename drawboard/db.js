75e24183185cc73180e077eea5c837455b0fe109
damarsidiq <damarsidiq@gmail.com>
2026-09-18 06:17:53 +0700

    storybasedDrawingBoard

--- drawboard/db.js
+++ drawboard/db.js
@@ -1,5 +1,5 @@
 export var qr = {
-    langsTotalDB:[585,0],
+    langsTotalDB:[641,0],
     langAvail:['-es','-de'],
     initPool:function(pdb){
         var tidx = qr.langAvail.indexOf(jve.foclang);
